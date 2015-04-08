264e943da6b3b624bcd2a20e7d5c8aef1d7d2cb1
Cyrill Schumacher <cyrill@schumacher.fm>
2015-04-08 20:31:32 +1000

    Update documentation

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 This repository contains the main framework.
 
-Please see the README files in the sub-packages.
+Please see [godoc.org](https://godoc.org/github.com/corestoreio/csfw) which is more up-to-date than this README.md file.
 
 Magento is a trademark of [MAGENTO, INC.](http://www.magentocommerce.com/license/).
 
 ## Badges
 
-[goreportcard](http://goreportcard.com/report/Corestoreio/csfw)
+[goreportcard](http://goreportcard.com/report/Corestoreio/csfw) [![GoDoc](https://godoc.org/github.com/corestoreio/csfw?status.svg)](https://godoc.org/github.com/corestoreio/csfw)
 
 @todo add travis
 
@@ -25,11 +25,6 @@ $ export CS_DSN='magento1:magento1@tcp(localhost:3306)/magento1'
 $ export CS_DSN='magento2:magento2@tcp(localhost:3306)/magento2'
 ```
 
-### Optional settings
-
-- `CS_EAV_MAP` the environment variable which points to a JSON file for mapping EAV entities.
-- ...
-
 ```
 $ go get github.com/corestoreio/csfw
 $ export CS_DSN_TEST='see next section'
@@ -39,7 +34,7 @@ $ go generate ./...
 
 ## Testing
 
-Setup two databases. One for Magento 1 and one for Magento and fill them.
+Setup two databases. One for Magento 1 and one for Magento 2 and fill them with the provided [test data](https://github.com/corestoreio/csfw/tree/master/testData).
 
 Create a DSN env var `CS_DSN_TEST` and point it to Magento 1 database. Run the tests.
 Change the env var to let it point to Magento 2 database. Rerun the tests.
@@ -48,6 +43,20 @@ Change the env var to let it point to Magento 2 database. Rerun the tests.
 $ export CS_DSN_TEST='magento1:magento1@tcp(localhost:3306)/magento1'
 $ export CS_DSN_TEST='magento2:magento2@tcp(localhost:3306)/magento2'
 ```
+
+## IDE
+
+Currently using the IntelliJ IDEA Community Edition with the [go-lang-idea-plugin](https://github.com/go-lang-plugin-org/go-lang-idea-plugin).
+At the moment Q2/2015: There are no official jar files for downloading so the go lang plugin will be 
+compiled on a daily basis. The plugin works very well!
+
+IDEA has been configured with goimports, gofmt, golint, govet and ... with the file watcher plugin.
+
+Within the Go files there are UML tags from [PlantUML](http://en.wikipedia.org/wiki/PlantUML) because you
+can have nice visualisations with the IDEA plugin of PlantUML. To run the PlantUML plugin you must 
+install: `$ brew install graphviz` on your OSX. For Linux ... ?
+
+Why am I not using vim? Because I would only generate passwords ;-|.
 
 ## Contributing
 
--- doc.go
+++ doc.go
@@ -13,8 +13,12 @@
 // limitations under the License.
 
 /*
-package csfw contains only go:generate commands to build go code. Some general coding
-styles and naming conventions.
+Package csfw contains the CoreStore FrameWork based on Magento's database structure.
+99% compatible to Magento 1 and 2.
+
+The package csfw contains at the moment only go:generate commands to build Go code.
+
+Two skeleton projects (monolith and SOA) are already setup but of course empty.
 
 Purpose
 
@@ -24,7 +28,7 @@ Because performance :-)
 
 Architecture
 
-...
+See the UML diagrams within the Go code. @todo 10km view.
 
 Names
 
@@ -46,6 +50,54 @@ Table indexes are iota constants and start with TableIndex[table name].
 
 The word "collection" will be appended to a variable or function when that variable or function contains
 a materialized slice or handles it.
+
+Required settings
+
+CS_DSN the environment variable for the MySQL connection.
+
+    $ export CS_DSN='magento1:magento1@tcp(localhost:3306)/magento1'
+    $ export CS_DSN='magento2:magento2@tcp(localhost:3306)/magento2'
+
+    $ go get github.com/corestoreio/csfw
+    $ export CS_DSN_TEST='see next section'
+    $ cd $GOPATH/src/github.com/corestoreio/csfw
+    $ go generate ./...
+
+Testing
+
+Setup two databases. One for Magento 1 and one for Magento 2 and fill them with
+the provided test data https://github.com/corestoreio/csfw/tree/master/testData
+
+Create a DSN env var CS_DSN_TEST and point it to Magento 1 database. Run the tests.
+Change the env var to let it point to Magento 2 database. Rerun the tests.
+
+    $ export CS_DSN_TEST='magento1:magento1@tcp(localhost:3306)/magento1'
+    $ export CS_DSN_TEST='magento2:magento2@tcp(localhost:3306)/magento2'
+
+IDE
+
+Currently using the IntelliJ IDEA Community Edition with the https://github.com/go-lang-plugin-org/go-lang-idea-plugin plugin.
+
+At the moment Q2/2015: There are no official jar files for downloading so the go lang plugin will be
+compiled on a daily basis. The plugin works very well! Kudos to those developers!
+
+IDEA has been configured with goimports, gofmt, golint, govet and ... with the file watcher plugin.
+
+Why am I not using vim? Because I would only generate passwords ;-|.
+
+UML
+
+Within the Go files there are UML tags from PlantUML http://en.wikipedia.org/wiki/PlantUML because you
+can have nice visualisations with the IDEA plugin of PlantUML. To run the PlantUML plugin you must
+install:
+
+    $ brew install graphviz
+
+on your OSX. For Unix/Linux ... ?
+
+Contributing
+
+Please have a look at the contribution guidelines https://github.com/corestoreio/corestore/blob/master/CONTRIBUTING.md
 
 Trademarks
 
