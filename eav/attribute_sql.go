cc47cd669710efd3aff5a4451c78eb7e3966bcdf
Cyrill Schumacher <cyrill@schumacher.fm>
2015-04-11 19:24:47 +1000

    Refactor GetAttributeSelectSql to include scope table

--- eav/attribute_sql.go
+++ eav/attribute_sql.go
@@ -17,6 +17,7 @@ package eav
 import (
 	"github.com/corestoreio/csfw/storage/csdb"
 	"github.com/corestoreio/csfw/storage/dbr"
+	"github.com/corestoreio/csfw/utils/stringSlice"
 	"github.com/juju/errgo"
 )
 
@@ -26,39 +27,6 @@ import (
 // @see magento2/app/code/Magento/Eav/Model/Resource/Attribute/Collection.php::_initSelect()
 func GetAttributeSelectSql(dbrSess dbr.SessionRunner, aat EntityTypeAdditionalAttributeTabler, entityTypeID, websiteId int64) (*dbr.SelectBuilder, error) {
 
-	/*
-			@todo
-		   SELECT
-		     `main_table`.`attribute_id`,
-		     `main_table`.`entity_type_id`,
-		     `main_table`.`attribute_code`,
-		     `main_table`.`backend_model`,
-		     `main_table`.`backend_type`,
-		     `main_table`.`backend_table`,
-		     `main_table`.`frontend_model`,
-		     `main_table`.`frontend_input`,
-		     `main_table`.`frontend_label`,
-		     `main_table`.`frontend_class`,
-		     `main_table`.`source_model`,
-		     `main_table`.`is_user_defined`,
-		     `main_table`.`is_unique`,
-		     `main_table`.`note`,
-		     `additional_table`.`input_filter`,
-		     `additional_table`.`validate_rules`,
-		     `additional_table`.`is_system`,
-		     `additional_table`.`sort_order`,
-		     `additional_table`.`data_model`,
-		     `additional_table`.`is_used_for_customer_segment`,
-		     IFNULL(`scope_table`.`is_required`, `main_table`.`is_required`)               AS `is_required`,
-		     IFNULL(`scope_table`.`default_value`, `main_table`.`default_value`)           AS `default_value`,
-		     IFNULL(`scope_table`.`is_visible`, `additional_table`.`is_visible`)           AS `is_visible`,
-		     IFNULL(`scope_table`.`multiline_count`, `additional_table`.`multiline_count`) AS `multiline_count`
-		   FROM `eav_attribute` AS `main_table` INNER JOIN `customer_eav_attribute` AS `additional_table`
-		       ON (`additional_table`.`attribute_id` = `main_table`.`attribute_id`) AND (`main_table`.`entity_type_id` = 1)
-		     LEFT JOIN `customer_eav_attribute_website` AS `scope_table`
-		       ON (`scope_table`.`attribute_id` = `main_table`.`attribute_id`) AND (`scope_table`.`website_id` = 4)
-	*/
-
 	ta, err := GetTableStructure(TableIndexAttribute)
 	if err != nil {
 		return nil, errgo.Mask(err)
@@ -67,39 +35,57 @@ func GetAttributeSelectSql(dbrSess dbr.SessionRunner, aat EntityTypeAdditionalAt
 	if err != nil {
 		return nil, errgo.Mask(err)
 	}
-
-	selectSql := dbrSess.
-		Select(ta.AllColumnAliasQuote(csdb.MainTable)...).
-		From(ta.Name, csdb.MainTable).
-		Join(
-		dbr.JoinTable(taa.Name, "additional_table"),
-		taa.ColumnAliasQuote("additional_table"),
-		dbr.JoinOn("`additional_table`.`attribute_id` = `main_table`.`attribute_id`"),
-		dbr.JoinOn("`main_table`.`entity_type_id` = ?", entityTypeID),
-	)
-
 	tew, err := aat.TableEavWebsite()
 	if err != nil {
 		return nil, errgo.Mask(err)
 	}
+	// tew table can now contains columns names which can occur in table eav_attribute and
+	// or [catalog|customer|entity]_eav_attribute
+	var (
+		ifnull           []string
+		tewAddedCols     []string
+		taColumnsQuoted  = stringSlice.Lot(ta.AllColumnAliasQuote(csdb.MainTable))
+		taaColumnsQuoted = stringSlice.Lot(taa.ColumnAliasQuote(csdb.AdditionalTable))
+	)
 
 	if tew != nil {
-		const scopeTable = "scope_table"
-		l := len(tew.Columns) * 2
-		cols := make([]string, l)
-		j := 0
-		for i := 0; i < l; i = i + 2 {
-			cols[i] = scopeTable + "." + tew.Columns[j] // real column name
-			cols[i+1] = "scope_" + tew.Columns[j]       // alias column name
-			j++
+		ifnull = make([]string, len(tew.Columns))
+		for i, tewC := range tew.Columns {
+			t := ""
+			switch {
+			case ta.In(tewC):
+				t = csdb.MainTable
+				break
+			case taa.In(tewC):
+				t = csdb.AdditionalTable
+				break
+			default:
+				return nil, errgo.Newf("Cannot find column name %s.%s neither in table %s nor in %s.", tew.Name, tewC, ta.Name, taa.Name)
+			}
+			ifnull[i] = dbr.IfNullAs(csdb.ScopeTable, tewC, t, tewC, tewC)
+			tewAddedCols = append(tewAddedCols, tewC)
 		}
+		taColumnsQuoted.FilterContains(tewAddedCols...)
+		taaColumnsQuoted.FilterContains(tewAddedCols...)
+	}
 
+	selectSql := dbrSess.
+		Select(taColumnsQuoted...).
+		From(ta.Name, csdb.MainTable).
+		Join(
+		dbr.JoinTable(taa.Name, csdb.AdditionalTable),
+		taaColumnsQuoted,
+		dbr.JoinOn("`"+csdb.AdditionalTable+"`.`attribute_id` = `"+csdb.MainTable+"`.`attribute_id`"),
+		dbr.JoinOn("`"+csdb.MainTable+"`.`entity_type_id` = ?", entityTypeID),
+	)
+
+	if len(tewAddedCols) > 0 {
 		selectSql.
 			LeftJoin(
-			dbr.JoinTable(tew.Name, "scope_table"),
-			dbr.ColumnAlias(cols...),
-			dbr.JoinOn("`scope_table`.`attribute_id` = `main_table`.`attribute_id`"),
-			dbr.JoinOn("`scope_table`.`website_id` = ?", websiteId),
+			dbr.JoinTable(tew.Name, csdb.ScopeTable),
+			ifnull,
+			dbr.JoinOn("`"+csdb.ScopeTable+"`.`attribute_id` = `"+csdb.MainTable+"`.`attribute_id`"),
+			dbr.JoinOn("`"+csdb.ScopeTable+"`.`website_id` = ?", websiteId),
 		)
 	}
 	return selectSql, nil
