16e3384cc9155cfd678e5e84396f3bc26f9bc23a
Cyrill Schumacher <cyrill@schumacher.fm>
2015-06-09 11:01:04 +1000

    Package money: Add Self-referential functions and the design of options

--- storage/money/currency.go
+++ storage/money/currency.go
@@ -13,20 +13,40 @@
 // limitations under the License.
 
 /*
-Package money usese a fixed-length guard for precision arithmetic.
+Package money uses a fixed-length guard for precision arithmetic.
 Implements Un/Marshaller and Scan() method for database
-columns including null. Optimized for usage: decimal(12,4) NULL
+columns including null, optimized for decimal(12, 4) fields.
 
 Rounding is done on float64 to int64 by	the Rnd() function truncating
 at values less than (.5 + (1 / Guardf))	or greater than -(.5 + (1 / Guardf))
 in the case of negative numbers. The Guard adds four decimal places
 of protection to rounding.
-DP is the decimal precision, which can be changed in the DecimalPrecision()
-function.  DP hold the places after the decimalplace in teh active money struct field M.
+Decimal precision can be changed in the Precision() option
+function. Precision() hold the places after the decimal place in teh active money struct field m.
 
 http://en.wikipedia.org/wiki/Floating_point#Accuracy_problems
 
-Initial copyright: Copyright (c) 2011 Jad Dittmar
+Options
+
+The following options can be set while calling New():
+
+	m := New(Swedish(Interval005), Guard(100), Precision(100))
+
+Those values are really optional and even the order they appear ;-).
+Default settings are:
+
+	Precision 10000 which reflects decimal(12,4) database field
+	Guard 	  10000 which reflects decimal(12,4) database field
+	Swedish   No rounding
+
+If you need to temporarily set a different option value you can stick to this pattern:
+http://commandcenter.blogspot.com/2014/01/self-referential-functions-and-design.html
+
+	prev := m.Option(Swedish(Interval005))
+	defer m.Option(prev)
+	// do something with the different Swedish rounding
+
+Initial Idea: Copyright (c) 2011 Jad Dittmar
 https://github.com/Confunctionist/finance
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -67,10 +87,13 @@ import (
 var (
 	ErrOverflow = errors.New("Integer Overflow")
 
-	guard  int64   = 100
-	guardf float64 = float64(guard)
-	dp     int64   = 10000 //  decimal(12,4)
-	dpf    float64 = float64(dp)
+	guard   int64   = 10000
+	guardi          = int(guard)
+	guardf  float64 = float64(guard)
+	dp      int64   = 10000
+	dpi             = int(dp)
+	dpf     float64 = float64(dp)
+	swedish         = Interval000
 
 	Round = .5
 	//	Round  = .5 + (1 / Guardf)
@@ -80,7 +103,7 @@ var (
 // Interval* constants http://en.wikipedia.org/wiki/Swedish_rounding
 const (
 	// Interval000 no swedish rounding (default)
-	Interval000 interval = iota
+	Interval000 Interval = iota
 	// Interval005 rounding with 0.05 intervals
 	Interval005
 	// Interval010 rounding with 0.10 intervals
@@ -89,38 +112,54 @@ const (
 	Interval050
 	// Interval100 rounding with 1.00 intervals
 	Interval100
+	// interval999 max, not available
+	interval999
 )
 
-// interval defines the internal type for the Swedish rounding.
-type interval uint8
+type (
+	// Interval defines the type for the Swedish rounding.
+	Interval uint8
 
-// Currency represents a money aka currency type to avoid rounding errors with floats.
-// Takes also care of http://en.wikipedia.org/wiki/Swedish_rounding
-type Currency struct {
-	// m money in Guard/DP
-	m int64
-	// Locale to allow language specific output formats @todo
-	Locale string
-	// Valid if false the internal value is NULL
-	Valid bool
-	// Swedish is the interval for the swedish rounding.
-	Swedish interval
+	// Currency represents a money aka currency type to avoid rounding errors with floats.
+	// Takes also care of http://en.wikipedia.org/wiki/Swedish_rounding
+	Currency struct {
+		// m money in Guard/DP
+		m int64
+		// Locale to allow language specific output formats @todo
+		Locale string
+		// Valid if false the internal value is NULL
+		Valid bool
+		// swedish is the interval for the swedish rounding.
+		swedish Interval
 
-	guard  int64
-	guardf float64
-	dp     int64
-	dpf    float64
+		guard  int64
+		guardf float64
+		dp     int64
+		dpf    float64
+	}
+
+	OptionFunc func(*Currency) OptionFunc
+)
+
+// DefaultSwedish sets the global and New() defaults swedish rounding
+// http://en.wikipedia.org/wiki/Swedish_rounding
+func DefaultSwedish(i Interval) {
+	if i < interval999 {
+		swedish = i
+	} else {
+		log.Error("money=SetSwedishRounding", "err", errors.New("Interval out of scope"), "interval", i)
+	}
 }
 
-// SetGuard sets the global and default for New() fixed-length guard for precision arithmetic
-func SetGuard(g int64) {
+// DefaultGuard sets the global and default for New() fixed-length guard for precision arithmetic
+func DefaultGuard(g int64) {
 	guard = g
 	guardf = float64(g)
 }
 
-// SetDecimalPrecision sets the global and default for New() decimal precision ... of 2 decimal places => 10^2 (can be reset)
+// DefaultPrecision sets the global and default for New() decimal precision ... of 2 decimal places => 10^2 (can be reset)
 // Package default value
-func SetDecimalPrecision(p int64) {
+func DefaultPrecision(p int64) {
 	p64 := int64(p)
 	l := int64(math.Log(float64(p64)))
 	if p64 == 0 || (p64 != 0 && (l%2) != 0) {
@@ -130,40 +169,75 @@ func SetDecimalPrecision(p int64) {
 	dpf = float64(p64)
 }
 
-// New creates a new empty Currency struct with package default values of Guard and DP.
-func New() Currency {
-	return Currency{
-		guard:  guard,
-		guardf: guardf,
-		dp:     dp,
-		dpf:    dpf,
+// Swedish sets the swedish rounding
+// http://en.wikipedia.org/wiki/Swedish_rounding
+func Swedish(i Interval) OptionFunc {
+	if i >= interval999 {
+		log.Error("Currency=SetSwedishRounding", "err", errors.New("Interval out of scope. Resetting."), "interval", i)
+		i = Interval000
+	}
+	return func(c *Currency) OptionFunc {
+		previous := c.swedish
+		c.swedish = i
+		return Swedish(previous)
 	}
 }
 
-// SetGuard sets the guard for the current struct.
-func (c Currency) SetGuard(g int) Currency {
-	if g < 1 {
+// SetGuard sets the guard
+func Guard(g int) OptionFunc {
+	if g == 0 { // check for division by zero
 		g = 1
 	}
-	c.guard = int64(g)
-	c.guardf = float64(g)
-	return c
+	return func(c *Currency) OptionFunc {
+		previous := int(c.guard)
+		c.guard = int64(g)
+		c.guardf = float64(g)
+		return Guard(previous)
+	}
 }
 
-// SetPrecision sets the precison for the current struct. 0, 10, 100, 1000 ...
-// If not a decimal power then falls back to the default value
-func (c Currency) SetPrecision(p int) Currency {
+// Precision sets the precison. 0, 10, 100, 1000 ...
+// If not a decimal power then falls back to the default value.
+func Precision(p int) OptionFunc {
 	p64 := int64(p)
 	l := int64(math.Log(float64(p64)))
 	if p64 != 0 && (l%2) != 0 {
 		p64 = dp
 	}
-	if p64 == 0 {
+	if p64 == 0 { // check for division by zero
 		p64 = 1
 	}
-	c.dp = p64
-	c.dpf = float64(p64)
+	return func(c *Currency) OptionFunc {
+		previous := int(c.dp)
+		c.dp = p64
+		c.dpf = float64(p64)
+		return Precision(previous)
+	}
+}
+
+// New creates a new empty Currency struct with package default values of
+// Guard and decimal precision.
+func New(opts ...OptionFunc) Currency {
+	c := Currency{
+		guard:  guard,
+		guardf: guardf,
+		dp:     dp,
+		dpf:    dpf,
+	}
+	c.Option(opts...)
 	return c
+}
+
+// Options besides New() also Option() can apply options to the current
+// struct. It returns the last set option. More info about the returned function:
+// http://commandcenter.blogspot.com/2014/01/self-referential-functions-and-design.html
+func (c *Currency) Option(opts ...OptionFunc) (previous OptionFunc) {
+	for _, o := range opts {
+		if o != nil {
+			previous = o(c)
+		}
+	}
+	return previous
 }
 
 // Abs Returns the absolute value of Currency
@@ -205,7 +279,7 @@ func (c Currency) Raw() int64 {
 	return c.m
 }
 
-// Set sets the Currency field M
+// Set sets the raw Currency field m
 func (c Currency) Set(i int64) Currency {
 	c.m = i
 	c.Valid = true
@@ -310,13 +384,17 @@ var (
 )
 
 func (c Currency) MarshalJSON() ([]byte, error) {
+	// @todo should be possible to output the value without the currency sign
+	// or output it as an array e.g.: [1234.56, "1.234,56€", "€"]
+	// hmmmm
 	if false == c.Valid {
 		return nullString, nil
 	}
-	return []byte(c.String()), nil
+	return []byte(`"` + c.String() + `"`), nil
 }
 
 func (c *Currency) UnmarshalJSON(b []byte) error {
+	// @todo rewrite and optimize unmarshalling but for now json.Unmarshal is fine
 	var s interface{}
 	if err := json.Unmarshal(b, &s); err != nil {
 		return err
@@ -324,14 +402,28 @@ func (c *Currency) UnmarshalJSON(b []byte) error {
 	return c.Scan(s)
 }
 
+// @todo quick write down without tests so add tests 8-)
 func (c *Currency) Scan(value interface{}) error {
 	if value == nil {
 		c.m, c.Valid = 0, false
 		return nil
 	}
-	c.Valid = true
-	// @todo
-	return nil //convertAssign(&n.Int64, value)
+	if c.guard == 0 {
+		c.Option(Guard(guardi))
+	}
+	if c.dp == 0 {
+		c.Option(Precision(dpi))
+	}
+
+	if rb, ok := value.(*sql.RawBytes); ok {
+		f, err := atof64([]byte(*rb))
+		if err != nil {
+			return log.Error("Currency=Scan", "err", err)
+		}
+		c.Valid = true
+		c.Setf(f)
+	}
+	return nil
 }
 
 var colon = []byte(",")
@@ -344,12 +436,12 @@ func atof64(bVal []byte) (f float64, err error) {
 	return f, err
 }
 
-//// ConvertValue @todo
+//// ConvertValue @todo ?
 //func (c Currency) ConvertValue(v interface{}) (driver.Value, error) {
 //	return nil, nil
 //}
 //
-//// Value @todo
+//// Value @todo ?
 //func (c Currency) Value() (driver.Value, error) {
 //	return nil, nil
 //}
