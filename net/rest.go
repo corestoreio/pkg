ec02f36b0c70f40547bb8f7044ac795a387b3721
Cyrill Schumacher <cyrill@schumacher.fm>
2015-08-15 18:40:50 +0200

    Add Package net

---
+++ net/rest.go
@@ -0,0 +1,58 @@
+// Copyright 2015, Cyrill @ Schumacher.fm and the CoreStore contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// net provides functions and configuration values for the network.
+package net
+
+import (
+	"net/http"
+
+	"github.com/corestoreio/csfw/utils/log"
+	"github.com/labstack/echo"
+)
+
+// APIRoute defines the current API version
+const APIRoute apiVersion = "/V1/"
+
+type apiVersion string
+
+// Versionize prepends the API version as defined in constant APIRoute to a route.
+func (a apiVersion) Versionize(r string) string {
+	if len(r) > 0 && r[:1] == "/" {
+		r = r[1:]
+	}
+	return string(a) + r
+}
+
+// String returns the current version and not the full route
+func (a apiVersion) String() string {
+	return string(a)
+}
+
+// RESTErrorHandler default REST error handler ... @todo remove echo dependency
+func RESTErrorHandler(err error, c *echo.Context) {
+	code := http.StatusInternalServerError
+	msg := http.StatusText(code)
+	if he, ok := err.(*echo.HTTPError); ok {
+		code = he.Code()
+		msg = he.Error()
+	}
+	if log.IsDebug() {
+		log.Error("net.RESTErrorHandler", "err", err)
+	}
+	msg = err.Error()
+
+	http.Error(c.Response(), msg, code)
+
+}
---
+++ net/rest_test.go
@@ -0,0 +1,38 @@
+// Copyright 2015, Cyrill @ Schumacher.fm and the CoreStore contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// net provides functions and configuration values for the network.
+package net_test
+
+import (
+	"testing"
+
+	"github.com/corestoreio/csfw/net"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVersionize(t *testing.T) {
+	tests := []struct {
+		have, want string
+	}{
+		{"login", "/V1/login"},
+		{"/login", "/V1/login"},
+		{"", "/V1/"},
+	}
+	for _, test := range tests {
+		h := net.APIRoute.Versionize(test.have)
+		assert.Equal(t, test.want, h)
+	}
+	assert.Equal(t, "/V1/", net.APIRoute.String())
+}
