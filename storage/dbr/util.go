cc0ee19f08f31f9f5572579f7a431d59131de76c
Cyrill Schumacher <cyrill@schumacher.fm>
2015-08-28 21:20:08 +0200

    storage/dbr: Add Stmt as stmtChecker

--- storage/dbr/util.go
+++ storage/dbr/util.go
@@ -2,6 +2,7 @@ package dbr
 
 import (
 	"database/sql/driver"
+	"strings"
 
 	"github.com/juju/errgo"
 )
@@ -41,4 +42,35 @@ func argsValuer(args *[]interface{}) error {
 		}
 	}
 	return nil
+}
+
+// Stmt is helper for various method to check statements
+var Stmt = stmtChecker{}
+
+// stmtChecker @todo better checking ...
+type stmtChecker struct{}
+
+func (stmtChecker) startContain(sql, starts, contains string) bool {
+	sql = strings.ToLower(sql)
+	return strings.Index(sql, starts) == 0 && strings.Index(sql, contains) > 4
+}
+
+// IsSelect checks if string is a SELECT statement
+func (sc stmtChecker) IsSelect(sql string) bool {
+	return sc.startContain(sql, "select", "from")
+}
+
+// IsUpdate checks if string is an UPDATE statement
+func (sc stmtChecker) IsUpdate(sql string) bool {
+	return sc.startContain(sql, "update", "from")
+}
+
+// IsDelete checks if string is a DELETE statement
+func (sc stmtChecker) IsDelete(sql string) bool {
+	return sc.startContain(sql, "delete", "from")
+}
+
+// IsInsert checks if string is an INSERT statement
+func (sc stmtChecker) IsInsert(sql string) bool {
+	return sc.startContain(sql, "insert", " ")
 }
---
+++ storage/dbr/util_test.go
@@ -0,0 +1,47 @@
+package dbr
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestStmtChecker(t *testing.T) {
+	tests := []struct {
+		sel   string
+		selok bool
+		upd   string
+		updok bool
+		del   string
+		delok bool
+		ins   string
+		insok bool
+	}{
+		{
+			"SELECT ...",
+			false,
+			"UPDATE ...",
+			false,
+			"DELETE ...",
+			false,
+			"INSERT",
+			false,
+		},
+		{
+			"SELECT ... From ",
+			true,
+			"UPDATE ... From ",
+			true,
+			"DELETE ...From ",
+			true,
+			"INSERT ",
+			true,
+		},
+	}
+
+	for _, test := range tests {
+		assert.Equal(t, test.selok, Stmt.IsSelect(test.sel), "%#v", test)
+		assert.Equal(t, test.updok, Stmt.IsUpdate(test.upd), "%#v", test)
+		assert.Equal(t, test.delok, Stmt.IsDelete(test.del), "%#v", test)
+		assert.Equal(t, test.insok, Stmt.IsInsert(test.ins), "%#v", test)
+	}
+}
