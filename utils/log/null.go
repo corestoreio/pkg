ec57f77947e9aa6503e0e8de11b933f0dc74e706
Cyrill Schumacher <cyrill@schumacher.fm>
2015-10-28 20:46:35 +0100

    utils/log: Remove pointer method receiver from NullLogger

--- utils/log/null.go
+++ utils/log/null.go
@@ -22,24 +22,24 @@ import "errors"
 type NullLogger struct{}
 
 // New returns a new Logger that has this logger's context plus the given context
-func (l *NullLogger) New(ctx ...interface{}) Logger { return &NullLogger{} }
+func (l NullLogger) New(ctx ...interface{}) Logger { return &NullLogger{} }
 
 // Trace logs a trace entry.
-func (l *NullLogger) Trace(msg string, args ...interface{}) {}
+func (l NullLogger) Trace(msg string, args ...interface{}) {}
 
 // Debug logs a debug entry.
-func (l *NullLogger) Debug(msg string, args ...interface{}) {}
+func (l NullLogger) Debug(msg string, args ...interface{}) {}
 
 // Info logs an info entry.
-func (l *NullLogger) Info(msg string, args ...interface{}) {}
+func (l NullLogger) Info(msg string, args ...interface{}) {}
 
 // Warn logs a warn entry.
-func (l *NullLogger) Warn(msg string, args ...interface{}) {}
+func (l NullLogger) Warn(msg string, args ...interface{}) {}
 
 // Error logs an error entry. Returns the first argument as an error OR
 // if the 2nd index of args (that is args[1] ;-) ) contains the error
 // then that error will be returned.
-func (l *NullLogger) Error(msg string, args ...interface{}) error {
+func (l NullLogger) Error(msg string, args ...interface{}) error {
 	lArgs := len(args)
 	if lArgs > 0 && lArgs%2 == 0 {
 		if err, ok := args[1].(error); ok {
@@ -50,22 +50,22 @@ func (l *NullLogger) Error(msg string, args ...interface{}) error {
 }
 
 // Fatal logs a fatal entry then panics.
-func (l *NullLogger) Fatal(msg string, args ...interface{}) { panic("exit due to fatal error") }
+func (l NullLogger) Fatal(msg string, args ...interface{}) { panic("exit due to fatal error") }
 
 // Log logs a leveled entry.
-func (l *NullLogger) Log(level int, msg string, args []interface{}) {}
+func (l NullLogger) Log(level int, msg string, args []interface{}) {}
 
 // IsTrace determines if this logger logs a trace statement.
-func (l *NullLogger) IsTrace() bool { return false }
+func (l NullLogger) IsTrace() bool { return false }
 
 // IsDebug determines if this logger logs a debug statement.
-func (l *NullLogger) IsDebug() bool { return false }
+func (l NullLogger) IsDebug() bool { return false }
 
 // IsInfo determines if this logger logs an info statement.
-func (l *NullLogger) IsInfo() bool { return false }
+func (l NullLogger) IsInfo() bool { return false }
 
 // IsWarn determines if this logger logs a warning statement.
-func (l *NullLogger) IsWarn() bool { return false }
+func (l NullLogger) IsWarn() bool { return false }
 
 // SetLevel sets the level of this logger.
-func (l *NullLogger) SetLevel(level int) {}
+func (l NullLogger) SetLevel(level int) {}
