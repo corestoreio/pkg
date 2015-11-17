003292a72b72d7bf38fa8c12bec1caeaa81bf0d8
Cyrill Schumacher <cyrill@schumacher.fm>
2015-11-17 20:07:25 +0100

    storage/dbr: Add TestQuoteAs with Benchmark
    BenchmarkQuoteAs-4	 3000000	       417 ns/op	      48 B/op	       2 allocs/op

---
+++ storage/dbr/quote_test.go
@@ -0,0 +1,35 @@
+package dbr
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestQuoteAs(t *testing.T) {
+	tests := []struct {
+		have []string
+		want string
+	}{
+		{[]string{"a"}, "`a`"},
+		{[]string{"a", "b"}, "`a` AS `b`"},
+		{[]string{"`c`"}, "`c`"},
+		{[]string{"d.e"}, "`d`.`e`"},
+		{[]string{"`d`.`e`"}, "`d`.`e`"},
+		{[]string{"f", "g", "h"}, "`f` AS `g_h`"},
+		{[]string{"f", "g", "h`h"}, "`f` AS `g_hh`"},
+	}
+	for i, test := range tests {
+		assert.Exactly(t, test.want, Quoter.QuoteAs(test.have...), "Index %d", i)
+	}
+}
+
+// BenchmarkQuoteAs-4	 3000000	       417 ns/op	      48 B/op	       2 allocs/op
+func BenchmarkQuoteAs(b *testing.B) {
+	want := "`e`.`entity_id` AS `ee`"
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if have := Quoter.QuoteAs("e.entity_id", "ee"); have != want {
+			b.Errorf("Have %s\nWant %s\n", have, want)
+		}
+	}
+}
