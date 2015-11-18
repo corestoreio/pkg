da2355ddd1d2ffedec96968343e3443b44b1d9b9
Cyrill Schumacher <cyrill@schumacher.fm>
2015-11-18 21:21:19 +0100

    utils/sqlbeautifier: Add basic SQL formatter

---
+++ utils/sqlbeautifier/sqlbeautifier_test.go
@@ -0,0 +1,65 @@
+// Copyright 2015, Cyrill @ Schumacher.fm and the CoreStore contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sqlbeautifier_test
+
+import (
+	"testing"
+
+	"bytes"
+	"fmt"
+	"io/ioutil"
+
+	"go/build"
+	"path/filepath"
+
+	"github.com/corestoreio/csfw/utils/sqlbeautifier"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFromReader(t *testing.T) {
+	tests := []struct {
+		wantErr error
+	}{
+		{nil},
+	}
+	for i, test := range tests {
+
+		haveF := getTestFile(t, i, "have")
+		wantF := getTestFile(t, i, "want")
+
+		bufFormat, haveErr := sqlbeautifier.FromReader(bytes.NewReader(haveF))
+		if test.wantErr != nil {
+			assert.Error(t, haveErr, "Index %d", i)
+			assert.EqualError(t, haveErr, test.wantErr.Error(), "Index %d", i)
+			continue
+		}
+
+		if bytes.Compare(wantF, bufFormat.Bytes()) != 0 {
+			// assert.NoError(t, ioutil.WriteFile(pathPrefix+".sql", bufFormat.Bytes(), 0644))
+			t.Errorf("Error @ Index %d\n%s\n", i, bufFormat.String())
+		}
+	}
+}
+
+var pathPrefix = filepath.Join(build.Default.GOPATH, "src", "github.com", "corestoreio", "csfw", "utils", "sqlbeautifier", "test_")
+
+func getTestFile(t *testing.T, i int, test string) []byte {
+	n := fmt.Sprintf("%s%02d_%s.sql", pathPrefix, i, test)
+	b, err := ioutil.ReadFile(n)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return b
+}
---
+++ utils/sqlbeautifier/test_00_want.sql
@@ -0,0 +1,50 @@
+select 
+	main_table.attribute_id, 
+	main_table.entity_type_id, 
+	main_table.attribute_code, 
+	main_table.attribute_model, 
+	main_table.backend_model, 
+	main_table.backend_type, 
+	main_table.backend_table, 
+	main_table.frontend_model, 
+	main_table.frontend_input, 
+	main_table.frontend_label, 
+	main_table.frontend_class, 
+	main_table.source_model, 
+	main_table.is_user_defined, 
+	main_table.is_unique, 
+	main_table.note, 
+	additional_table.input_filter, 
+	additional_table.validate_rules, 
+	additional_table.is_system, 
+	additional_table.sort_order, 
+	additional_table.data_model, 
+	ifnull(
+	scope_table.is_visible, 
+	additional_table.is_visible 
+) as is_visible, 
+	ifnull(
+	scope_table.is_required, 
+	main_table.is_required 
+) as is_required, 
+	ifnull(
+	scope_table.default_value, 
+	main_table.default_value 
+) as default_value, 
+	ifnull(
+	scope_table.multiline_count, 
+	additional_table.multiline_count 
+) as multiline_count 
+ from 
+	eav_attribute as main_table join 
+	customer_eav_attribute as additional_table on 
+		(additional_table.attribute_id = main_table.attribute_id) and 
+		(main_table.entity_type_id = :v1)
+ left join 
+	customer_eav_attribute_website as scope_table on 
+		(scope_table.attribute_id = main_table.attribute_id) and 
+		(scope_table.website_id = :v2)
+ where multiline_count > 0
+ order by 
+	main_table.attribute_id asc
+ limit 12, 13
