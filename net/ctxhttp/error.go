b378a01e65887eadee36c0717829ddf71749c7c3
Cyrill Schumacher <cyrill@schumacher.fm>
2015-12-08 08:34:02 +0100

    net/ctxhttp: Add Error type for status code and message

---
+++ net/ctxhttp/error.go
@@ -0,0 +1,55 @@
+// Copyright 2015, Cyrill @ Schumacher.fm and the CoreStore contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ctxhttp
+
+import (
+	"net/http"
+
+	"github.com/corestoreio/csfw/utils"
+)
+
+// Error is a HTTP error with a messgae and a status code
+type Error struct {
+	// Code displays the status code
+	Code int
+	// Message contains human readable information
+	Message string
+}
+
+// NewError creates a new Error. If msg will be passed one time then
+// the StatusText of the code will be overridden.
+func NewError(code int, msg ...string) *Error {
+	e := &Error{Code: code, Message: http.StatusText(code)}
+	if len(msg) > 0 && msg[0] != "" {
+		e.Message = msg[0]
+	}
+	return e
+}
+
+// NewErrorFromErrors this lovely name describes that it can create
+// a HTTP error from multiple error interfaces. The function utils.Errors()
+// will be used to extract the errgo.Locationer interface.
+func NewErrorFromErrors(code int, errs ...error) *Error {
+	e := &Error{Code: code, Message: http.StatusText(code)}
+	if len(errs) > 0 {
+		e.Message = utils.Errors(errs...)
+	}
+	return e
+}
+
+// Error returns message.
+func (e *Error) Error() string {
+	return e.Message
+}
---
+++ net/ctxhttp/error_test.go
@@ -0,0 +1,58 @@
+// Copyright 2015, Cyrill @ Schumacher.fm and the CoreStore contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ctxhttp_test
+
+import (
+	"net/http"
+	"testing"
+
+	"errors"
+	"github.com/corestoreio/csfw/net/ctxhttp"
+	"github.com/juju/errgo"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewError(t *testing.T) {
+	tests := []struct {
+		code      int
+		msg       []string
+		wantError string
+	}{
+		{http.StatusBadGateway, nil, http.StatusText(http.StatusBadGateway)},
+		{http.StatusTeapot, []string{"No coffee pot", "ignored"}, "No coffee pot"},
+	}
+	for _, test := range tests {
+		he := ctxhttp.NewError(test.code, test.msg...)
+		assert.Exactly(t, test.code, he.Code)
+		assert.Exactly(t, test.wantError, he.Error())
+	}
+}
+
+func TestNewErrorFromErrors(t *testing.T) {
+	tests := []struct {
+		code      int
+		errs      []error
+		wantError string
+	}{
+		{http.StatusBadGateway, nil, http.StatusText(http.StatusBadGateway)},
+		{http.StatusTeapot, []error{errors.New("No coffee pot"), errors.New("Not even a milk pot")}, "No coffee pot\nNot even a milk pot"},
+		{http.StatusConflict, []error{errgo.New("Now a coffee pot"), errgo.New("Not even close to a milk pot")}, "error_test.go"},
+	}
+	for _, test := range tests {
+		he := ctxhttp.NewErrorFromErrors(test.code, test.errs...)
+		assert.Exactly(t, test.code, he.Code)
+		assert.Contains(t, he.Error(), test.wantError)
+	}
+}
