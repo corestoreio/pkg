e4abc1296ec662466621f5c47f645dcf0bbb2ea9
Cyrill Schumacher <cyrill@schumacher.fm>
2015-12-21 20:54:25 +0100

    util/bufferpool: Implement average bufferpool size

--- util/bufferpool/bufpool.go
+++ util/bufferpool/bufpool.go
@@ -18,9 +18,15 @@ import (
 	"sync"
 )
 
+// AverageBufferSize should be adjusted to the average size of a bytes.buffer
+// in your application
+var AverageBufferSize int = 16
+
 var bufferPool = &sync.Pool{
 	New: func() interface{} {
-		return &bytes.Buffer{}
+		b := bytes.NewBuffer(make([]byte, AverageBufferSize))
+		b.Reset()
+		return b
 	},
 }
 
@@ -32,6 +38,7 @@ func Get() (buf *bytes.Buffer) {
 // Put returns a buffer to the pool.
 // The buffer is reset before it is put back into circulation.
 func Put(buf *bytes.Buffer) {
+	// println(buf.Len()) todo for some statistics
 	buf.Reset()
 	bufferPool.Put(buf)
 }
