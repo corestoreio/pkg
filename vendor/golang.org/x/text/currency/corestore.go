90f66a9661377f4f9b97e4e6e0ea117bd908d40e
Cyrill Schumacher <cyrill@schumacher.fm>
2015-12-26 19:53:37 +0100

    vendor/text/currency: Add CS specific files and features

---
+++ vendor/golang.org/x/text/currency/corestore.go
@@ -0,0 +1,38 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:generate go run gen.go gen_common.go -output tables.go
+
+// Package currency contains currency-related functionality.
+//
+// NOTE: the formatting functionality is currently under development and may
+// change without notice.
+package currency // import "golang.org/x/text/currency"
+
+// AllCurrencies returns all available currencies on this planet.
+func AllCurrencies() []string {
+	all := make([]string, numCurrencies)
+	for i := 0; i < numCurrencies; i++ {
+		all[i] = currency.Elem(i + 1)[:3]
+	}
+	return all
+}
+
+type regionUnit struct {
+	Region string
+	Unit
+}
+
+// AllRegionsWithUnit returns all regions with its assigned Unit
+func AllRegionsWithUnit() []regionUnit {
+	all := make([]regionUnit, len(regionToCurrency))
+	for i := 0; i < len(regionToCurrency); i++ {
+		r := regionToCurrency[i].region
+		all[i] = regionUnit{
+			Region: string(r>>8) + string(r^((r>>8)<<8)),
+			Unit:   Unit{regionToCurrency[i].code},
+		}
+	}
+	return all
+}
---
+++ vendor/golang.org/x/text/currency/corestore_test.go
@@ -0,0 +1,26 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package currency
+
+import "testing"
+
+func TestAllCurrencies(t *testing.T) {
+	all := AllCurrencies()
+	if c := all[0]; c != "ADP" {
+		t.Errorf("first was %c; want ADP", c)
+	}
+	if c := all[len(all)-1]; c != "ZWR" {
+		t.Errorf("last was %c; want ZWR", c)
+	}
+}
+
+func TestAllRegionsWithUnit(t *testing.T) {
+	all := AllRegionsWithUnit()
+	for i := 0; i < len(all); i++ {
+		if have := all[i].Unit.String(); all[i].Region == "DE" && have != EUR.String() {
+			t.Errorf("DE have currency %s; want EUR", have)
+		}
+	}
+}
