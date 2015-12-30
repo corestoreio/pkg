ae38c8d889f54f0b4507fc5e8469a64786892fe7
Cyrill Schumacher <cyrill@schumacher.fm>
2015-12-30 18:11:17 +0100

    config: Move elements into its own package

--- backend/doc.go
+++ backend/doc.go
@@ -12,12 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/*
-Package backend ...
-
-The Backend module contains common infrastructure and assets for other modules
-to be defined and used in their administration user interface (UI). It does
-not contain anything specific to other modules. Among many things it
-handles the logic of authenticating and authorizing users.
-*/
+// Package backend provides common configuration and functions.
+//
+// The Backend module contains common infrastructure and assets for other modules
+// to be defined and used in their administration user interface (UI). It does
+// not contain anything specific to other modules. Among many things it
+// handles the logic of authenticating and authorizing users.
 package backend
