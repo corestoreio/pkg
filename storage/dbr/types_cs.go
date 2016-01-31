de48e197666b6f915b14a712346f0582b3ac28f6
Cyrill Schumacher <cyrill@schumacher.fm>
2016-01-31 14:37:35 +0100

    storage/dbr: Remove github.com/ugorji/go/codec because file size
    binaries gets blowed up by 5MB. will be added back if needed

--- storage/dbr/types_cs.go
+++ storage/dbr/types_cs.go
@@ -16,15 +16,13 @@ package dbr
 
 import (
 	"fmt"
-
 	"strings"
 
-	"github.com/ugorji/go/codec"
+	// "github.com/ugorji/go/codec"
 )
 
-var (
-	_ codec.Selfer = (*NullString)(nil)
-)
+// removed because the binary grows for 5MB. will be added once needed.
+// var _ codec.Selfer = (*NullString)(nil)
 
 // GoString satisfies the interface fmt.GoStringer when using %#v in Printf methods.
 // Returns
@@ -43,73 +41,73 @@ func (ns NullString) GoString() string {
 	return fmt.Sprintf("dbr.NewNullString(%s)", ns.String)
 }
 
-// CodecEncodeSelf for ugorji.go codec package
-func (n NullString) CodecEncodeSelf(e *codec.Encoder) {
-	if err := e.Encode(n.String); err != nil {
-		PkgLog.Debug("dbr.NullString.CodecEncodeSelf", "err", err, "n", n)
-	}
-}
-
-// CodecDecodeSelf  for ugorji.go codec package @todo write test ... not sure if ok
-func (n *NullString) CodecDecodeSelf(d *codec.Decoder) {
-	if err := d.Decode(&n.String); err != nil {
-		PkgLog.Debug("dbr.NullString.CodecDecodeSelf", "err", err, "n", n)
-	}
-	// think about empty string and Valid value ...
-}
-
-// CodecEncodeSelf for ugorji.go codec package
-func (n *NullInt64) CodecEncodeSelf(e *codec.Encoder) {
-	if err := e.Encode(n.Int64); err != nil {
-		PkgLog.Debug("dbr.NullInt64.CodecEncodeSelf", "err", err, "n", n)
-	}
-}
-
-// CodecDecodeSelf  for ugorji.go codec package @todo write test ... not sure if ok
-func (n *NullInt64) CodecDecodeSelf(d *codec.Decoder) {
-	if err := d.Decode(&n.Int64); err != nil {
-		PkgLog.Debug("dbr.NullInt64.CodecDecodeSelf", "err", err, "n", n)
-	}
-}
-
-// CodecEncodeSelf for ugorji.go codec package
-func (n NullFloat64) CodecEncodeSelf(e *codec.Encoder) {
-	if err := e.Encode(n.Float64); err != nil {
-		PkgLog.Debug("dbr.NullFloat64.CodecEncodeSelf", "err", err, "n", n)
-	}
-}
-
-// CodecDecodeSelf  for ugorji.go codec package @todo write test ... not sure if ok
-func (n *NullFloat64) CodecDecodeSelf(d *codec.Decoder) {
-	if err := d.Decode(&n.Float64); err != nil {
-		PkgLog.Debug("dbr.NullFloat64.CodecDecodeSelf", "err", err, "n", n)
-	}
-}
-
-// CodecEncodeSelf for ugorji.go codec package
-func (n NullTime) CodecEncodeSelf(e *codec.Encoder) {
-	if err := e.Encode(n.Time); err != nil {
-		PkgLog.Debug("dbr.NullTime.CodecEncodeSelf", "err", err, "n", n)
-	}
-}
-
-// CodecDecodeSelf  for ugorji.go codec package @todo write test ... not sure if ok
-func (n *NullTime) CodecDecodeSelf(d *codec.Decoder) {
-	if err := d.Decode(&n.Time); err != nil {
-		PkgLog.Debug("dbr.NullTime.CodecDecodeSelf", "err", err, "n", n)
-	}
-}
-
-// CodecEncodeSelf for ugorji.go codec package
-func (n NullBool) CodecEncodeSelf(e *codec.Encoder) {
-	if err := e.Encode(n.Bool); err != nil {
-		PkgLog.Debug("dbr.NullBool.CodecEncodeSelf", "err", err, "n", n)
-	}
-}
-
-// CodecDecodeSelf  for ugorji.go codec package @todo write test ... not sure if ok
-func (n *NullBool) CodecDecodeSelf(d *codec.Decoder) {
-	if err := d.Decode(&n.Bool); err != nil {
-		PkgLog.Debug("dbr.NullBool.CodecDecodeSelf", "err", err, "n", n)
-	}
-}
+//// CodecEncodeSelf for ugorji.go codec package
+//func (n NullString) CodecEncodeSelf(e *codec.Encoder) {
+//	if err := e.Encode(n.String); err != nil {
+//		PkgLog.Debug("dbr.NullString.CodecEncodeSelf", "err", err, "n", n)
+//	}
+//}
+//
+//// CodecDecodeSelf  for ugorji.go codec package @todo write test ... not sure if ok
+//func (n *NullString) CodecDecodeSelf(d *codec.Decoder) {
+//	if err := d.Decode(&n.String); err != nil {
+//		PkgLog.Debug("dbr.NullString.CodecDecodeSelf", "err", err, "n", n)
+//	}
+//	// think about empty string and Valid value ...
+//}
+//
+//// CodecEncodeSelf for ugorji.go codec package
+//func (n *NullInt64) CodecEncodeSelf(e *codec.Encoder) {
+//	if err := e.Encode(n.Int64); err != nil {
+//		PkgLog.Debug("dbr.NullInt64.CodecEncodeSelf", "err", err, "n", n)
+//	}
+//}
+//
+//// CodecDecodeSelf  for ugorji.go codec package @todo write test ... not sure if ok
+//func (n *NullInt64) CodecDecodeSelf(d *codec.Decoder) {
+//	if err := d.Decode(&n.Int64); err != nil {
+//		PkgLog.Debug("dbr.NullInt64.CodecDecodeSelf", "err", err, "n", n)
+//	}
+//}
+//
+//// CodecEncodeSelf for ugorji.go codec package
+//func (n NullFloat64) CodecEncodeSelf(e *codec.Encoder) {
+//	if err := e.Encode(n.Float64); err != nil {
+//		PkgLog.Debug("dbr.NullFloat64.CodecEncodeSelf", "err", err, "n", n)
+//	}
+//}
+//
+//// CodecDecodeSelf  for ugorji.go codec package @todo write test ... not sure if ok
+//func (n *NullFloat64) CodecDecodeSelf(d *codec.Decoder) {
+//	if err := d.Decode(&n.Float64); err != nil {
+//		PkgLog.Debug("dbr.NullFloat64.CodecDecodeSelf", "err", err, "n", n)
+//	}
+//}
+//
+//// CodecEncodeSelf for ugorji.go codec package
+//func (n NullTime) CodecEncodeSelf(e *codec.Encoder) {
+//	if err := e.Encode(n.Time); err != nil {
+//		PkgLog.Debug("dbr.NullTime.CodecEncodeSelf", "err", err, "n", n)
+//	}
+//}
+//
+//// CodecDecodeSelf  for ugorji.go codec package @todo write test ... not sure if ok
+//func (n *NullTime) CodecDecodeSelf(d *codec.Decoder) {
+//	if err := d.Decode(&n.Time); err != nil {
+//		PkgLog.Debug("dbr.NullTime.CodecDecodeSelf", "err", err, "n", n)
+//	}
+//}
+//
+//// CodecEncodeSelf for ugorji.go codec package
+//func (n NullBool) CodecEncodeSelf(e *codec.Encoder) {
+//	if err := e.Encode(n.Bool); err != nil {
+//		PkgLog.Debug("dbr.NullBool.CodecEncodeSelf", "err", err, "n", n)
+//	}
+//}
+//
+//// CodecDecodeSelf  for ugorji.go codec package @todo write test ... not sure if ok
+//func (n *NullBool) CodecDecodeSelf(d *codec.Decoder) {
+//	if err := d.Decode(&n.Bool); err != nil {
+//		PkgLog.Debug("dbr.NullBool.CodecDecodeSelf", "err", err, "n", n)
+//	}
+//}
