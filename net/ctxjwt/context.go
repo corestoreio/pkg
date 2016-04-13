532e982e96bd9017b0d666c8a61b97f821e73c42
Cyrill Schumacher <cyrill@schumacher.fm>
2016-04-13 20:31:29 +0200

    net/ctxjwt: After cpu profiling optimize With/From Context functions
    reduce calls runtime.convT2E

--- net/ctxjwt/context.go
+++ net/ctxjwt/context.go
@@ -19,34 +19,39 @@ import (
 	"golang.org/x/net/context"
 )
 
-type (
-	keyCtxToken struct{}
-	keyCtxErr   struct{}
-)
+type keyCtxToken struct{}
+
+type ctxTokenWrapper struct {
+	t   csjwt.Token
+	err error
+}
 
 // withContext creates a new context with csjwt.Token attached.
 func withContext(ctx context.Context, t csjwt.Token) context.Context {
-	return context.WithValue(ctx, keyCtxToken{}, t)
+	return context.WithValue(ctx, keyCtxToken{}, ctxTokenWrapper{t: t})
 }
 
 // FromContext returns the csjwt.Token in ctx if it exists or an error.
 // If there is no token in the context then the error
 // ErrContextJWTNotFound gets returned.
-func FromContext(ctx context.Context) (t csjwt.Token, err error) {
-	var ok bool
-	err, ok = ctx.Value(keyCtxErr{}).(error)
-	if ok && err != nil {
-		return
+func FromContext(ctx context.Context) (csjwt.Token, error) {
+
+	wrp, ok := ctx.Value(keyCtxToken{}).(ctxTokenWrapper)
+	if !ok {
+		return wrp.t, ErrContextJWTNotFound
 	}
-	err = ErrContextJWTNotFound
-	t, ok = ctx.Value(keyCtxToken{}).(csjwt.Token)
-	if ok && len(t.Raw) > 5 && t.Valid {
-		err = nil
+
+	if wrp.err != nil {
+		return wrp.t, wrp.err
 	}
-	return
+
+	if wrp.t.Valid {
+		return wrp.t, nil
+	}
+	return wrp.t, ErrContextJWTNotFound
 }
 
 // withContextError creates a new context with an error attached.
 func withContextError(ctx context.Context, err error) context.Context {
-	return context.WithValue(ctx, keyCtxErr{}, err)
+	return context.WithValue(ctx, keyCtxToken{}, ctxTokenWrapper{err: err})
 }
