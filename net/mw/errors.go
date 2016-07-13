416a760adacc7cfa31a4b59ae5519bdd58a0988c
Cyrill Schumacher <cyrill@schumacher.fm>
2016-07-13 07:16:00 +0200

    net/mw: Add type ErrorHandler and ErrorWithStatusCode()
    ErrorHandler passes an error to an handler and returns the handler with the
    wrapped error.
    
    ErrorWithStatusCode wraps an HTTP Status Code into an ErrorHandler. The
    status text message gets printed first followed by the verbose error string.
    This may leak sensitive information.

---
+++ net/mw/errors.go
@@ -0,0 +1,35 @@
+// Copyright 2015-2016, Cyrill @ Schumacher.fm and the CoreStore contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mw
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// ErrorHandler passes an error to an handler and returns the handler with the
+// wrapped error.
+type ErrorHandler func(error) http.Handler
+
+// ErrorWithStatusCode wraps an HTTP Status Code into an ErrorHandler. The
+// status text message gets printed first followed by the verbose error string.
+// This may leak sensitive information.
+func ErrorWithStatusCode(code int) ErrorHandler {
+	return func(err error) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+			http.Error(w, fmt.Sprintf("%s\n\n%+v", http.StatusText(code), err), code)
+		})
+	}
+}
