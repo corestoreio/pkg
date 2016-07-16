691ed59428bbe0ee4bedb06a93efed552742126e
Cyrill Schumacher <cyrill@schumacher.fm>
2016-07-16 13:36:04 +0200

    net/auth: Add ACL todos

--- net/auth/acl.go
+++ net/auth/acl.go
@@ -1,3 +1,7 @@
 package auth
 
-// todo use package authorization to handle roles and rules
+// todo use package authorization to handle roles and rules but do not import the package rather define in package auth an
+// interface which requires some functions for role and rule handling
+
+// https://blog.komand.com/quick-security-wins-in-golang
+// https://github.com/goji/httpauth/blob/master/basic_auth.go
