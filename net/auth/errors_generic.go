d1139d0571c8f79b7acdc654df9607d24d19c3e8
Cyrill Schumacher <cyrill@schumacher.fm>
2016-08-31 08:03:46 +0200

    net/auth: Add generated code

---
+++ net/auth/errors_generic.go
@@ -0,0 +1,21 @@
+// Copyright 2015-2016, Cyrill @ Schumacher.fm and the CoreStore contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package auth
+
+import "github.com/corestoreio/csfw/util/errors"
+
+// Auto generated: Do not edit. See net/internal/scopedService package for more details.
+
+var errConfigNotFound = errors.NewNotFoundf(`[auth] ScopedConfig not available`)
