146ba77da08aa6fb6fdb2e8d5b3289f56efb7f4c
Cyrill Schumacher <cyrill@schumacher.fm>
2016-09-04 10:43:29 +0200

    storage: Update doc to add some ideas

--- storage/doc.go
+++ storage/doc.go
@@ -15,8 +15,35 @@
 /*
 Package storage provides everything from MySQL, Redis, BoltDB, file, etc functions.
 
-TODO(cs): github.com/youtube/vitess/go/mysql we should be compatible with this raw MYSQL driver.
-TODO: use https://github.com/rubenv/sql-migrate for database migrations
-	https://github.com/rubenv/sql-migrate is better because the API for using it within your code (FOSDEM2016).
+DB Schema Go Generation
+
+bitbucket.org/jatone/genieql
+sql query and code generation program. its purpose is to generate as much of the
+boilerplate code for interacting with database/sql as possible. without putting
+any runtime dependencies into your codebase. it only supports postgresql
+currently. adding additional support is very straight forward, just implement
+the Dialect interface. see the postgresql implementation as the example.
+
+github.com/go-reform/reform
+A better ORM for Go, based on non-empty interfaces and code generation.
+https://gopkg.in/reform.v1
+
+
+DB Migrations
+
+github.com/rubenv/sql-migrate 500 Stars
+SQL Schema migration tool for Go. Based on gorp and goose.
+Is better because the API for using it within your code (FOSDEM2016).
+
+https://bitbucket.org/liamstask/goose
+goose is a database migration tool. You can manage your database's evolution by
+creating incremental SQL or Go scripts.
+Cons: https://www.reddit.com/r/golang/comments/2dlbz5/database_migration_handling_in_go/
+
+github.com/mattes/migrate 744 Stars
+A migration helper written in Go. Use it in your existing Golang code or run
+commands via the CLI.
+
+
 */
 package storage
