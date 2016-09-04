ec7f121b91093f711bda4b273b547735893050cc
Cyrill Schumacher <cyrill@schumacher.fm>
2016-09-04 16:16:10 +0200

    net/csrf: Add idea to implement this package

---
+++ net/csrf/doc.go
@@ -0,0 +1,20 @@
+// Copyright 2015-2016, Cyrill @ Schumacher.fm and the CoreStore contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package csrf implements scope based Cross-Site Request Forgery protection.
+//
+// Sub-package `backendcsrf` implements the external configuration loading.
+//
+// http://stackoverflow.com/questions/20504846/why-is-it-common-to-put-csrf-prevention-tokens-in-cookies/20518324#20518324
+package csrf
