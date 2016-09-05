bbc02cdd2023ecfcbbbddad113052917c9ef3998
Cyrill Schumacher <cyrill@schumacher.fm>
2016-09-05 17:01:52 +0200

    net/responseproxy: Add tee writer (part of Goji)
    Copyright (c) 2014, 2015, 2016 Carl Jackson (carl@avtok.com)
    
    MIT License
    
    Permission is hereby granted, free of charge, to any person obtaining a copy of
    this software and associated documentation files (the "Software"), to deal in
    the Software without restriction, including without limitation the rights to
    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
    the Software, and to permit persons to whom the Software is furnished to do so,
    subject to the following conditions:
    
    The above copyright notice and this permission notice shall be included in all
    copies or substantial portions of the Software.
    
    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.

---
+++ net/responseproxy/tee_writer_test.go
@@ -0,0 +1,39 @@
+// Copyright 2015-2016, Cyrill @ Schumacher.fm and the CoreStore contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package responseproxy_test
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/corestoreio/csfw/net/responseproxy"
+	"github.com/stretchr/testify/assert"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestWrapTee(t *testing.T) {
+
+	tw := responseproxy.WrapTee(httptest.NewRecorder())
+	buf := new(bytes.Buffer)
+	tw.Tee(buf)
+
+	data := []byte(`“It can be very dangerous to see things from somebody else's point of view without the proper training.” - Douglas Adams`)
+	n, err := tw.Write(data)
+	assert.Exactly(t, len(data), n)
+	assert.Exactly(t, len(data), tw.BytesWritten())
+	assert.NoError(t, err)
+	assert.Exactly(t, string(data), buf.String())
+	assert.Contains(t, fmt.Sprintf("%#v", tw), `responseproxy.flushWriter`) // poor mans type testing ;-)
+}
