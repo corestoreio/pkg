696b9bbed4da62ea9abb96548ef6ec51727ce951
Cyrill Schumacher <cyrill@schumacher.fm>
2016-09-21 21:22:01 +0200

    util/errors: Fix tests

--- util/errors/multierr_test.go
+++ util/errors/multierr_test.go
@@ -35,7 +35,7 @@ func TestMultiErrors(t *testing.T) {
 	).Error()
 
 	assert.Regexp(t,
-		"Err1\ngithub.com/corestoreio/csfw/util/errors_test.TestMultiErrors\n\t.+/github.com/corestoreio/csfw/util/errors/multierr_test.go:32\ntesting.tRunner\n\t/usr/local/opt/go/libexec/src/testing/testing.go:610\nErr2\ngithub.com/corestoreio/csfw/util/errors_test.TestMultiErrors\n\t.+/github.com/corestoreio/csfw/util/errors/multierr_test.go:33\ntesting.tRunner\n\t/usr/local/opt/go/libexec/src/testing/testing.go:610\nErr3\ngithub.com/corestoreio/csfw/util/errors_test.TestMultiErrors\n\t.+/github.com/corestoreio/csfw/util/errors/multierr_test.go:34\ntesting.tRunner\n\t.+/testing/testing.go:610\n",
+		"Err1\ngithub.com/corestoreio/csfw/util/errors_test.TestMultiErrors\n\t.+/github.com/corestoreio/csfw/util/errors/multierr_test.go:32\ntesting.tRunner\n\t/usr/local/opt/go/libexec/src/testing/testing.go:[0-9]+\nErr2\ngithub.com/corestoreio/csfw/util/errors_test.TestMultiErrors\n\t.+/github.com/corestoreio/csfw/util/errors/multierr_test.go:33\ntesting.tRunner\n\t/usr/local/opt/go/libexec/src/testing/testing.go:[0-9]+\nErr3\ngithub.com/corestoreio/csfw/util/errors_test.TestMultiErrors\n\t.+/github.com/corestoreio/csfw/util/errors/multierr_test.go:34\ntesting.tRunner\n\t.+/testing/testing.go:[0-9]+\n",
 		errStr,
 	)
 }
@@ -50,7 +50,7 @@ func TestMultiAppend(t *testing.T) {
 	)
 	assert.True(t, e.HasErrors())
 	assert.Regexp(t,
-		"Err5\ngithub.com/corestoreio/csfw/util/errors_test.TestMultiAppend\n\t.+/github.com/corestoreio/csfw/util/errors/multierr_test.go:46\ntesting.tRunner\n\t/usr/local/opt/go/libexec/src/testing/testing.go:610\nErr6\ngithub.com/corestoreio/csfw/util/errors_test.TestMultiAppend\n\t.+/github.com/corestoreio/csfw/util/errors/multierr_test.go:48\ntesting.tRunner\n\t/usr/local/opt/go/libexec/src/testing/testing.go:610\nErr7\ngithub.com/corestoreio/csfw/util/errors_test.TestMultiAppend\n\t.+/github.com/corestoreio/csfw/util/errors/multierr_test.go:49\ntesting.tRunner\n\t.+/testing/testing.go:610\n",
+		"Err5\ngithub.com/corestoreio/csfw/util/errors_test.TestMultiAppend\n\t.+/github.com/corestoreio/csfw/util/errors/multierr_test.go:46\ntesting.tRunner\n\t/usr/local/opt/go/libexec/src/testing/testing.go:[0-9]+\nErr6\ngithub.com/corestoreio/csfw/util/errors_test.TestMultiAppend\n\t.+/github.com/corestoreio/csfw/util/errors/multierr_test.go:48\ntesting.tRunner\n\t/usr/local/opt/go/libexec/src/testing/testing.go:[0-9]+\nErr7\ngithub.com/corestoreio/csfw/util/errors_test.TestMultiAppend\n\t.+/github.com/corestoreio/csfw/util/errors/multierr_test.go:49\ntesting.tRunner\n\t.+/testing/testing.go:[0-9]+\n",
 		e.Error(),
 	)
 }
@@ -76,7 +76,7 @@ func TestMultiAppendToNil(t *testing.T) {
 	e = e.AppendErrors(errors.New("Err74"))
 
 	assert.True(t, e.HasErrors())
-	assert.Regexp(t, "Err74\ngithub.com/corestoreio/csfw/util/errors_test.TestMultiAppendToNil\n\t.+/github.com/corestoreio/csfw/util/errors/multierr_test.go:76\ntesting.tRunner\n\t.+/testing/testing.go:610\n", e.Error())
+	assert.Regexp(t, "Err74\ngithub.com/corestoreio/csfw/util/errors_test.TestMultiAppendToNil\n\t.+/github.com/corestoreio/csfw/util/errors/multierr_test.go:76\ntesting.tRunner\n\t.+/testing/testing.go:[0-9]+\n", e.Error())
 }
 
 func xxxTestMultiErr_CustomFormatter(t *testing.T) {
@@ -90,7 +90,7 @@ func xxxTestMultiErr_CustomFormatter(t *testing.T) {
 	)
 
 	assert.Regexp(t,
-		"Hello1\ngithub.com/corestoreio/csfw/util/errors_test.TestMultiErr_CustomFormatter\n\t.+/github.com/corestoreio/csfw/util/errors/multierr_test.go:84\ntesting.tRunner\n\t/usr/local/opt/go/libexec/src/testing/testing.go:610\nAlready closed\ngithub.com/corestoreio/csfw/util/errors.NewAlreadyClosedf\n\t.+/github.com/corestoreio/csfw/util/errors/behaviour.go:414: Brain\nNot found\ngithub.com/corestoreio/csfw/util/errors.NewNotFoundf\n\t.+/github.com/corestoreio/csfw/util/errors/behaviour.go:234: Mind\nHello2\ngithub.com/corestoreio/csfw/util/errors_test.TestMultiErr_CustomFormatter\n\t.+/github.com/corestoreio/csfw/util/errors/multierr_test.go:89\ntesting.tRunner\n\t.+/testing/testing.go:610\n",
+		"Hello1\ngithub.com/corestoreio/csfw/util/errors_test.TestMultiErr_CustomFormatter\n\t.+/github.com/corestoreio/csfw/util/errors/multierr_test.go:84\ntesting.tRunner\n\t/usr/local/opt/go/libexec/src/testing/testing.go:[0-9]+\nAlready closed\ngithub.com/corestoreio/csfw/util/errors.NewAlreadyClosedf\n\t.+/github.com/corestoreio/csfw/util/errors/behaviour.go:414: Brain\nNot found\ngithub.com/corestoreio/csfw/util/errors.NewNotFoundf\n\t.+/github.com/corestoreio/csfw/util/errors/behaviour.go:234: Mind\nHello2\ngithub.com/corestoreio/csfw/util/errors_test.TestMultiErr_CustomFormatter\n\t.+/github.com/corestoreio/csfw/util/errors/multierr_test.go:89\ntesting.tRunner\n\t.+/testing/testing.go:[0-9]+\n",
 		m1.Error())
 
 	m1.Formatter = func(errs []error) string {
