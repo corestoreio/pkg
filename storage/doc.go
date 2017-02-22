c65df24cdf47db276db90a9cf50f9a237db82b18
Cyrill Schumacher <cyrill@schumacher.fm>
2017-02-22 07:49:57 +0100

    storage: Update documentation

--- storage/doc.go
+++ storage/doc.go
@@ -12,38 +12,22 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/*
-Package storage provides everything from MySQL, Redis, BoltDB, file, etc functions.
-
-DB Schema Go Generation
-
-bitbucket.org/jatone/genieql
-sql query and code generation program. its purpose is to generate as much of the
-boilerplate code for interacting with database/sql as possible. without putting
-any runtime dependencies into your codebase. it only supports postgresql
-currently. adding additional support is very straight forward, just implement
-the Dialect interface. see the postgresql implementation as the example.
-
-github.com/go-reform/reform
-A better ORM for Go, based on non-empty interfaces and code generation.
-https://gopkg.in/reform.v1
-
-
-DB Migrations
-
-github.com/rubenv/sql-migrate 500 Stars
-SQL Schema migration tool for Go. Based on gorp and goose.
-Is better because the API for using it within your code (FOSDEM2016).
-
-https://bitbucket.org/liamstask/goose
-goose is a database migration tool. You can manage your database's evolution by
-creating incremental SQL or Go scripts.
-Cons: https://www.reddit.com/r/golang/comments/2dlbz5/database_migration_handling_in_go/
-
-github.com/mattes/migrate 744 Stars
-A migration helper written in Go. Use it in your existing Golang code or run
-commands via the CLI.
-
-
-*/
+// Package storage provides everything from MySQL, Redis, BoltDB, file etc
+// functions.
+//
+// DB Schema Go Generation
+//
+// Will be written on our own as all code generation tools have some serious
+// architectural flaw. See package codegen.
+//
+// bitbucket.org/jatone/genieql
+// sql query and code generation program. its purpose is to generate as much of the
+// boilerplate code for interacting with database/sql as possible. without putting
+// any runtime dependencies into your codebase. it only supports postgresql
+// currently. adding additional support is very straight forward, just implement
+// the Dialect interface. see the postgresql implementation as the example.
+//
+// github.com/go-reform/reform
+// A better ORM for Go, based on non-empty interfaces and code generation.
+// https://gopkg.in/reform.v1
 package storage
