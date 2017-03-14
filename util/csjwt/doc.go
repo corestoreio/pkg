b4b6e2f238491a4fbcc36fa0d6890a91afb9932b
Cyrill Schumacher <cyrill@schumacher.fm>
2017-03-14 18:26:05 +0100

    util/csjwt: Investigate security bugs critical-vulnerability-uncovered
    http://blogs.adobe.com/security/2017/03/critical-vulnerability-uncovered-in-json-encryption.html

--- util/csjwt/doc.go
+++ util/csjwt/doc.go
@@ -22,4 +22,16 @@
 //
 // https://news.ycombinator.com/item?id=11929267 => For people using JWT as a
 // substitute for stateful sessions, how do you handle renewal (or revocation)?
+//
+// A new discussion: https://news.ycombinator.com/item?id=13865459 JSON Web
+// Tokens should be avoided (paragonie.com)
+//
+// TODO: Investigate security bugs: http://blogs.adobe.com/security/2017/03/critical-vulnerability-uncovered-in-json-encryption.html
+// Critical Vulnerability Uncovered in JSON Encryption. Executive Summary: If
+// you are using go-jose, node-jose, jose2go, Nimbus JOSE+JWT or jose4 with
+// ECDH-ES please update to the latest version. RFC 7516 aka JSON Web Encryption
+// (JWE) Invalid Curve Attack. This can allow an attacker to recover the secret
+// key of a party using JWE with Key Agreement with Elliptic Curve
+// Diffie-Hellman Ephemeral Static (ECDH-ES), where the sender could extract
+// receiver’s private key.
 package csjwt
