4d8d8b8db82b63856e3202630feecd26b47acc2c
Cyrill Schumacher <cyrill@schumacher.fm>
2017-04-09 09:55:25 +0200

    util/null: Move NullBytes to package storage/dbr

--- util/null/bytes_test.go
+++ util/null/bytes_test.go
@@ -1,184 +1,185 @@
 package null
 
-import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"testing"
-)
-
-var (
-	bytesJSON = []byte(`"hello"`)
-)
-
-func TestBytesFrom(t *testing.T) {
-	i := BytesFrom([]byte(`"hello"`))
-	assertBytes(t, i, "BytesFrom()")
-
-	zero := BytesFrom(nil)
-	if zero.Valid {
-		t.Error("BytesFrom(nil)", "is valid, but should be invalid")
-	}
-
-	zero = BytesFrom([]byte{})
-	if !zero.Valid {
-		t.Error("BytesFrom([]byte{})", "is invalid, but should be valid")
-	}
-}
-
-func TestBytesFromPtr(t *testing.T) {
-	n := []byte(`"hello"`)
-	iptr := &n
-	i := BytesFromPtr(iptr)
-	assertBytes(t, i, "BytesFromPtr()")
-
-	null := BytesFromPtr(nil)
-	assertNullBytes(t, null, "BytesFromPtr(nil)")
-}
-
-func TestUnmarshalBytes(t *testing.T) {
-	var i Bytes
-	err := json.Unmarshal(bytesJSON, &i)
-	maybePanic(err)
-	assertBytes(t, i, "[]byte json")
-
-	var ni Bytes
-	err = ni.UnmarshalJSON([]byte{})
-	if ni.Valid == false {
-		t.Errorf("expected Valid to be true, got false")
-	}
-	if !bytes.Equal(ni.Bytes, []byte("null")) {
-		t.Errorf("Expected Bytes to be nil slice, but was not: %#v %#v", ni.Bytes, []byte(`null`))
-	}
-
-	var null Bytes
-	err = null.UnmarshalJSON(nil)
-	if null.Valid == false {
-		t.Errorf("expected Valid to be true, got false")
-	}
-	if !bytes.Equal(null.Bytes, []byte(`null`)) {
-		t.Errorf("Expected Bytes to be []byte nil, but was not: %#v %#v", null.Bytes, []byte(`null`))
-	}
-}
-
-func TestTextUnmarshalBytes(t *testing.T) {
-	var i Bytes
-	err := i.UnmarshalText([]byte(`"hello"`))
-	maybePanic(err)
-	assertBytes(t, i, "UnmarshalText() []byte")
-
-	var blank Bytes
-	err = blank.UnmarshalText([]byte(""))
-	maybePanic(err)
-	assertNullBytes(t, blank, "UnmarshalText() empty []byte")
-}
-
-func TestMarshalBytes(t *testing.T) {
-	i := BytesFrom([]byte(`"hello"`))
-	data, err := json.Marshal(i)
-	maybePanic(err)
-	assertJSONEquals(t, data, `"hello"`, "non-empty json marshal")
-
-	// invalid values should be encoded as null
-	null := MakeBytes(nil, false)
-	data, err = json.Marshal(null)
-	maybePanic(err)
-	assertJSONEquals(t, data, "null", "null json marshal")
-}
-
-func TestMarshalBytesText(t *testing.T) {
-	i := BytesFrom([]byte(`"hello"`))
-	data, err := i.MarshalText()
-	maybePanic(err)
-	assertJSONEquals(t, data, `"hello"`, "non-empty text marshal")
-
-	// invalid values should be encoded as null
-	null := MakeBytes(nil, false)
-	data, err = null.MarshalText()
-	maybePanic(err)
-	assertJSONEquals(t, data, "", "null text marshal")
-}
-
-func TestBytesPointer(t *testing.T) {
-	i := BytesFrom([]byte(`"hello"`))
-	ptr := i.Ptr()
-	if !bytes.Equal(*ptr, []byte(`"hello"`)) {
-		t.Errorf("bad %s []byte: %#v ≠ %s\n", "pointer", ptr, `"hello"`)
-	}
-
-	null := MakeBytes(nil, false)
-	ptr = null.Ptr()
-	if ptr != nil {
-		t.Errorf("bad %s []byte: %#v ≠ %s\n", "nil pointer", ptr, "nil")
-	}
-}
-
-func TestBytesIsZero(t *testing.T) {
-	i := BytesFrom([]byte(`"hello"`))
-	if i.IsZero() {
-		t.Errorf("IsZero() should be false")
-	}
-
-	null := MakeBytes(nil, false)
-	if !null.IsZero() {
-		t.Errorf("IsZero() should be true")
-	}
-
-	zero := MakeBytes(nil, true)
-	if zero.IsZero() {
-		t.Errorf("IsZero() should be false")
-	}
-}
-
-func TestBytesSetValid(t *testing.T) {
-	change := MakeBytes(nil, false)
-	assertNullBytes(t, change, "SetValid()")
-	change.SetValid([]byte(`"hello"`))
-	assertBytes(t, change, "SetValid()")
-}
-
-func TestBytesScan(t *testing.T) {
-	var i Bytes
-	err := i.Scan(`"hello"`)
-	maybePanic(err)
-	assertBytes(t, i, "scanned []byte")
-
-	var null Bytes
-	err = null.Scan(nil)
-	maybePanic(err)
-	assertNullBytes(t, null, "scanned null")
-}
-
-func assertBytes(t *testing.T, i Bytes, from string) {
-	if !bytes.Equal(i.Bytes, []byte(`"hello"`)) {
-		t.Errorf("bad %s []byte: %#v ≠ %#v\n", from, string(i.Bytes), string([]byte(`"hello"`)))
-	}
-	if !i.Valid {
-		t.Error(from, "is invalid, but should be valid")
-	}
-}
-
-func assertNullBytes(t *testing.T, i Bytes, from string) {
-	if i.Valid {
-		t.Error(from, "is valid, but should be invalid")
-	}
-}
-
-var _ fmt.GoStringer = (*Bytes)(nil)
-
-func TestBytes_GoString(t *testing.T) {
-	b := MakeBytes([]byte("test"), true)
-	if have, want := b.GoString(), "null.BytesFrom([]byte{0x74, 0x65, 0x73, 0x74})"; have != want {
-		t.Errorf("Have: %v Want: %v", have, want)
-	}
-	b = MakeBytes([]byte("test"), false)
-	if have, want := b.GoString(), "null.Bytes{}"; have != want {
-		t.Errorf("Have: %v Want: %v", have, want)
-	}
-	b = MakeBytes([]byte("te`st"), true)
-	// null.MakeString(`te`+"`"+`st`)
-	gsWant := []byte(`null.BytesFrom([]byte{0x74, 0x65, 0x60, 0x73, 0x74})`)
-	if !bytes.Equal(gsWant, []byte(b.GoString())) {
-		t.Errorf("Have: %#v Want: %v", b.GoString(), string(gsWant))
-	}
-}
+//
+//import (
+//	"bytes"
+//	"encoding/json"
+//	"fmt"
+//	"testing"
+//)
+//
+//var (
+//	bytesJSON = []byte(`"hello"`)
+//)
+//
+//func TestBytesFrom(t *testing.T) {
+//	i := BytesFrom([]byte(`"hello"`))
+//	assertBytes(t, i, "BytesFrom()")
+//
+//	zero := BytesFrom(nil)
+//	if zero.Valid {
+//		t.Error("BytesFrom(nil)", "is valid, but should be invalid")
+//	}
+//
+//	zero = BytesFrom([]byte{})
+//	if !zero.Valid {
+//		t.Error("BytesFrom([]byte{})", "is invalid, but should be valid")
+//	}
+//}
+//
+//func TestBytesFromPtr(t *testing.T) {
+//	n := []byte(`"hello"`)
+//	iptr := &n
+//	i := BytesFromPtr(iptr)
+//	assertBytes(t, i, "BytesFromPtr()")
+//
+//	null := BytesFromPtr(nil)
+//	assertNullBytes(t, null, "BytesFromPtr(nil)")
+//}
+//
+//func TestUnmarshalBytes(t *testing.T) {
+//	var i Bytes
+//	err := json.Unmarshal(bytesJSON, &i)
+//	maybePanic(err)
+//	assertBytes(t, i, "[]byte json")
+//
+//	var ni Bytes
+//	err = ni.UnmarshalJSON([]byte{})
+//	if ni.Valid == false {
+//		t.Errorf("expected Valid to be true, got false")
+//	}
+//	if !bytes.Equal(ni.Bytes, []byte("null")) {
+//		t.Errorf("Expected Bytes to be nil slice, but was not: %#v %#v", ni.Bytes, []byte(`null`))
+//	}
+//
+//	var null Bytes
+//	err = null.UnmarshalJSON(nil)
+//	if null.Valid == false {
+//		t.Errorf("expected Valid to be true, got false")
+//	}
+//	if !bytes.Equal(null.Bytes, []byte(`null`)) {
+//		t.Errorf("Expected Bytes to be []byte nil, but was not: %#v %#v", null.Bytes, []byte(`null`))
+//	}
+//}
+//
+//func TestTextUnmarshalBytes(t *testing.T) {
+//	var i Bytes
+//	err := i.UnmarshalText([]byte(`"hello"`))
+//	maybePanic(err)
+//	assertBytes(t, i, "UnmarshalText() []byte")
+//
+//	var blank Bytes
+//	err = blank.UnmarshalText([]byte(""))
+//	maybePanic(err)
+//	assertNullBytes(t, blank, "UnmarshalText() empty []byte")
+//}
+//
+//func TestMarshalBytes(t *testing.T) {
+//	i := BytesFrom([]byte(`"hello"`))
+//	data, err := json.Marshal(i)
+//	maybePanic(err)
+//	assertJSONEquals(t, data, `"hello"`, "non-empty json marshal")
+//
+//	// invalid values should be encoded as null
+//	null := MakeBytes(nil, false)
+//	data, err = json.Marshal(null)
+//	maybePanic(err)
+//	assertJSONEquals(t, data, "null", "null json marshal")
+//}
+//
+//func TestMarshalBytesText(t *testing.T) {
+//	i := BytesFrom([]byte(`"hello"`))
+//	data, err := i.MarshalText()
+//	maybePanic(err)
+//	assertJSONEquals(t, data, `"hello"`, "non-empty text marshal")
+//
+//	// invalid values should be encoded as null
+//	null := MakeBytes(nil, false)
+//	data, err = null.MarshalText()
+//	maybePanic(err)
+//	assertJSONEquals(t, data, "", "null text marshal")
+//}
+//
+//func TestBytesPointer(t *testing.T) {
+//	i := BytesFrom([]byte(`"hello"`))
+//	ptr := i.Ptr()
+//	if !bytes.Equal(*ptr, []byte(`"hello"`)) {
+//		t.Errorf("bad %s []byte: %#v ≠ %s\n", "pointer", ptr, `"hello"`)
+//	}
+//
+//	null := MakeBytes(nil, false)
+//	ptr = null.Ptr()
+//	if ptr != nil {
+//		t.Errorf("bad %s []byte: %#v ≠ %s\n", "nil pointer", ptr, "nil")
+//	}
+//}
+//
+//func TestBytesIsZero(t *testing.T) {
+//	i := BytesFrom([]byte(`"hello"`))
+//	if i.IsZero() {
+//		t.Errorf("IsZero() should be false")
+//	}
+//
+//	null := MakeBytes(nil, false)
+//	if !null.IsZero() {
+//		t.Errorf("IsZero() should be true")
+//	}
+//
+//	zero := MakeBytes(nil, true)
+//	if zero.IsZero() {
+//		t.Errorf("IsZero() should be false")
+//	}
+//}
+//
+//func TestBytesSetValid(t *testing.T) {
+//	change := MakeBytes(nil, false)
+//	assertNullBytes(t, change, "SetValid()")
+//	change.SetValid([]byte(`"hello"`))
+//	assertBytes(t, change, "SetValid()")
+//}
+//
+//func TestBytesScan(t *testing.T) {
+//	var i Bytes
+//	err := i.Scan(`"hello"`)
+//	maybePanic(err)
+//	assertBytes(t, i, "scanned []byte")
+//
+//	var null Bytes
+//	err = null.Scan(nil)
+//	maybePanic(err)
+//	assertNullBytes(t, null, "scanned null")
+//}
+//
+//func assertBytes(t *testing.T, i Bytes, from string) {
+//	if !bytes.Equal(i.Bytes, []byte(`"hello"`)) {
+//		t.Errorf("bad %s []byte: %#v ≠ %#v\n", from, string(i.Bytes), string([]byte(`"hello"`)))
+//	}
+//	if !i.Valid {
+//		t.Error(from, "is invalid, but should be valid")
+//	}
+//}
+//
+//func assertNullBytes(t *testing.T, i Bytes, from string) {
+//	if i.Valid {
+//		t.Error(from, "is valid, but should be invalid")
+//	}
+//}
+//
+//var _ fmt.GoStringer = (*Bytes)(nil)
+//
+//func TestBytes_GoString(t *testing.T) {
+//	b := MakeBytes([]byte("test"), true)
+//	if have, want := b.GoString(), "null.BytesFrom([]byte{0x74, 0x65, 0x73, 0x74})"; have != want {
+//		t.Errorf("Have: %v Want: %v", have, want)
+//	}
+//	b = MakeBytes([]byte("test"), false)
+//	if have, want := b.GoString(), "null.Bytes{}"; have != want {
+//		t.Errorf("Have: %v Want: %v", have, want)
+//	}
+//	b = MakeBytes([]byte("te`st"), true)
+//	// null.MakeString(`te`+"`"+`st`)
+//	gsWant := []byte(`null.BytesFrom([]byte{0x74, 0x65, 0x60, 0x73, 0x74})`)
+//	if !bytes.Equal(gsWant, []byte(b.GoString())) {
+//		t.Errorf("Have: %#v Want: %v", b.GoString(), string(gsWant))
+//	}
+//}
