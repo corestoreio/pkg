7b86990c859be7e28526e2c3545b4af50170a6c9
Cyrill Schumacher <cyrill@schumacher.fm>
2017-04-21 10:04:59 +0200

    eav: add IfNull function from package dbr
    Because IfNull is specific in EAV scope cases

---
+++ eav/sql.go
@@ -0,0 +1,64 @@
+// Copyright 2015-2017, Cyrill @ Schumacher.fm and the CoreStore contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package eav
+
+import "github.com/corestoreio/csfw/util/bufferpool"
+
+// DefaultScopeNames specifies the name of the scopes used in all EAV* function
+// to generate scope based hierarchical fall backs.
+var DefaultScopeNames = [...]string{"Store", "Group", "Website", "Default"}
+
+// IfNull creates a nested IFNULL SQL statement when a scope based fall back
+// hierarchy is required. Alias argument will be used as a prefix for the alias
+// table name and as the final alias name.
+func IfNull(alias, columnName, defaultVal string, scopeNames ...string) string {
+	buf := bufferpool.Get()
+	defer bufferpool.Put(buf)
+
+	if len(scopeNames) == 0 {
+		scopeNames = DefaultScopeNames[:]
+	}
+
+	brackets := 0
+	for _, n := range scopeNames {
+		buf.WriteString("IFNULL(")
+		buf.WriteRune('`')
+		buf.WriteString(alias)
+		buf.WriteString(n)
+		buf.WriteRune('`')
+		buf.WriteRune('.')
+		buf.WriteRune('`')
+		buf.WriteString(columnName)
+		buf.WriteRune('`')
+		if brackets < len(scopeNames)-1 {
+			buf.WriteRune(',')
+		}
+		brackets++
+	}
+
+	if defaultVal == "" {
+		defaultVal = `''`
+	}
+	buf.WriteRune(',')
+	buf.WriteString(defaultVal)
+	for i := 0; i < brackets; i++ {
+		buf.WriteRune(')')
+	}
+	buf.WriteString(" AS ")
+	buf.WriteRune('`')
+	buf.WriteString(alias)
+	buf.WriteRune('`')
+	return buf.String()
+}
---
+++ eav/sql_test.go
@@ -0,0 +1,87 @@
+// Copyright 2015-2017, Cyrill @ Schumacher.fm and the CoreStore contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package eav_test
+
+import (
+	"testing"
+
+	"github.com/corestoreio/csfw/eav"
+	"github.com/corestoreio/csfw/storage/dbr"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIfNull(t *testing.T) {
+	tests := []struct {
+		alias      string
+		columnName string
+		defaultVal string
+		want       string
+	}{
+		{
+			"manufacturer", "value", "",
+			"IFNULL(`manufacturerStore`.`value`,IFNULL(`manufacturerGroup`.`value`,IFNULL(`manufacturerWebsite`.`value`,IFNULL(`manufacturerDefault`.`value`,'')))) AS `manufacturer`",
+		},
+		{
+			"manufacturer", "value", "0",
+			"IFNULL(`manufacturerStore`.`value`,IFNULL(`manufacturerGroup`.`value`,IFNULL(`manufacturerWebsite`.`value`,IFNULL(`manufacturerDefault`.`value`,0)))) AS `manufacturer`",
+		},
+	}
+	for i, test := range tests {
+		assert.Exactly(t, test.want, eav.IfNull(test.alias, test.columnName, test.defaultVal), "Index %d", i)
+	}
+}
+
+func TestSelect_Join_EAVIfNull(t *testing.T) {
+	t.Parallel()
+	const want = "SELECT IFNULL(`manufacturerStore`.`value`,IFNULL(`manufacturerGroup`.`value`,IFNULL(`manufacturerWebsite`.`value`,IFNULL(`manufacturerDefault`.`value`,'')))) AS `manufacturer`, cpe.* FROM `catalog_product_entity` AS `cpe` LEFT JOIN `catalog_product_entity_varchar` AS `manufacturerDefault` ON (manufacturerDefault.scope = 0) AND (manufacturerDefault.scope_id = 0) AND (manufacturerDefault.attribute_id = 83) AND (manufacturerDefault.value IS NOT NULL) LEFT JOIN `catalog_product_entity_varchar` AS `manufacturerWebsite` ON (manufacturerWebsite.scope = 1) AND (manufacturerWebsite.scope_id = 10) AND (manufacturerWebsite.attribute_id = 83) AND (manufacturerWebsite.value IS NOT NULL) LEFT JOIN `catalog_product_entity_varchar` AS `manufacturerGroup` ON (manufacturerGroup.scope = 2) AND (manufacturerGroup.scope_id = 20) AND (manufacturerGroup.attribute_id = 83) AND (manufacturerGroup.value IS NOT NULL) LEFT JOIN `catalog_product_entity_varchar` AS `manufacturerStore` ON (manufacturerStore.scope = 2) AND (manufacturerStore.scope_id = 20) AND (manufacturerStore.attribute_id = 83) AND (manufacturerStore.value IS NOT NULL)"
+
+	s := dbr.NewSelect(eav.IfNull("manufacturer", "value", "''"), "cpe.*").
+		From("catalog_product_entity", "cpe").
+		LeftJoin(
+			dbr.MakeAlias("catalog_product_entity_varchar", "manufacturerDefault"),
+			dbr.Condition("manufacturerDefault.scope = 0"),
+			dbr.Condition("manufacturerDefault.scope_id = 0"),
+			dbr.Condition("manufacturerDefault.attribute_id = 83"),
+			dbr.Condition("manufacturerDefault.value IS NOT NULL"),
+		).
+		LeftJoin(
+			dbr.MakeAlias("catalog_product_entity_varchar", "manufacturerWebsite"),
+			dbr.Condition("manufacturerWebsite.scope = 1"),
+			dbr.Condition("manufacturerWebsite.scope_id = 10"),
+			dbr.Condition("manufacturerWebsite.attribute_id = 83"),
+			dbr.Condition("manufacturerWebsite.value IS NOT NULL"),
+		).
+		LeftJoin(
+			dbr.MakeAlias("catalog_product_entity_varchar", "manufacturerGroup"),
+			dbr.Condition("manufacturerGroup.scope = 2"),
+			dbr.Condition("manufacturerGroup.scope_id = 20"),
+			dbr.Condition("manufacturerGroup.attribute_id = 83"),
+			dbr.Condition("manufacturerGroup.value IS NOT NULL"),
+		).
+		LeftJoin(
+			dbr.MakeAlias("catalog_product_entity_varchar", "manufacturerStore"),
+			dbr.Condition("manufacturerStore.scope = 2"),
+			dbr.Condition("manufacturerStore.scope_id = 20"),
+			dbr.Condition("manufacturerStore.attribute_id = 83"),
+			dbr.Condition("manufacturerStore.value IS NOT NULL"),
+		)
+
+	sql, _, err := s.ToSQL()
+	assert.NoError(t, err)
+	assert.Equal(t,
+		want,
+		sql,
+	)
+}
