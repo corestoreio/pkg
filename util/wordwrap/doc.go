7714710f3adf50d522022152fa6dae1365481b13
Cyrill Schumacher <cyrill@schumacher.fm>
2017-04-22 12:28:22 +0200

    util/wordwrap: Add this package. Wraps long text after n chars
    The MIT License (MIT)
    
    Copyright (c) 2014 Mitchell Hashimoto
    
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
    in the Software without restriction, including without limitation the rights
    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
    copies of the Software, and to permit persons to whom the Software is
    furnished to do so, subject to the following conditions:
    
    The above copyright notice and this permission notice shall be included in
    all copies or substantial portions of the Software.
    
    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
    THE SOFTWARE.

---
+++ util/wordwrap/doc.go
@@ -0,0 +1,4 @@
+// Package wordwrap is a package for Go that automatically wraps words into multiple lines.
+// The primary use case for this is in formatting CLI output,
+// but of course word wrapping is a generally useful thing to do.
+package wordwrap
