1800e4b9915bbe51b853c7ceffc699f32bf80361
Cyrill Schumacher <cyrill@schumacher.fm>
2017-06-25 21:43:39 +0200

    util/php: Remove all packages. Sorry for adding them 8-(

--- util/php/phpsession/common.go
+++
@@ -1,7 +0,0 @@
-package phpsession
-
-import "github.com/corestoreio/csfw/util/php/phpserialize"
-
-const SEPARATOR_VALUE_NAME rune = '|'
-
-type PhpSession map[string]phpserialize.PhpValue
--- util/php/phpsession/doc.go
+++
@@ -1,2 +0,0 @@
-// Package phpsession provides possibility to decode/encode php session data in php_binary format.
-package phpsession
