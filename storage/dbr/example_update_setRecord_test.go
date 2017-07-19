31aa4f253d98144ef26f969be1aa539a844625b1
Cyrill Schumacher <cyrill@schumacher.fm>
2017-07-19 07:30:39 +0200

    storage/dbr: ExampleUpdate_SetRecord add complex struct field
    To show encoding of that field when running an UPDATE query.

--- storage/dbr/example_update_setRecord_test.go
+++ storage/dbr/example_update_setRecord_test.go
@@ -16,6 +16,7 @@ package dbr_test
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/corestoreio/csfw/storage/dbr"
 	"github.com/corestoreio/errors"
@@ -30,6 +31,9 @@ type categoryEntity struct {
 	AttributeSetID int64
 	ParentID       string
 	Path           dbr.NullString
+	// TeaserIDs contain a list of foreign primary keys which identifies special
+	// teaser to be shown on the category page.
+	TeaserIDs []string
 }
 
 func (pe *categoryEntity) AppendArguments(stmtType int, args dbr.Arguments, columns []string) (dbr.Arguments, error) {
@@ -43,6 +47,16 @@ func (pe *categoryEntity) AppendArguments(stmtType int, args dbr.Arguments, colu
 			args = append(args, dbr.String(pe.ParentID))
 		case "path":
 			args = append(args, pe.Path)
+		case "teaser_id_s":
+			if stmtType&dbr.SQLPartSet != 0 {
+				if pe.TeaserIDs == nil {
+					args = append(args, nil)
+				} else {
+					args = append(args, dbr.String(strings.Join(pe.TeaserIDs, "|")))
+				}
+			} else {
+				args = append(args, dbr.Strings(pe.TeaserIDs))
+			}
 		default:
 			return nil, errors.NewNotFoundf("[dbr_test] Column %q not found", c)
 		}
@@ -52,22 +66,22 @@ func (pe *categoryEntity) AppendArguments(stmtType int, args dbr.Arguments, colu
 
 func ExampleUpdate_SetRecord() {
 
-	ce := &categoryEntity{345, 6, "p123", dbr.MakeNullString("4/5/6/7")}
+	ce := &categoryEntity{345, 6, "p123", dbr.MakeNullString("4/5/6/7"), []string{"saleAutumn", "saleShoe"}}
 
 	// Updates all rows in the table
 	u := dbr.NewUpdate("catalog_category_entity").
-		AddColumns("attribute_set_id", "parent_id", "path").
+		AddColumns("attribute_set_id", "parent_id", "path", "teaser_id_s").
 		SetRecord(ce)
 	writeToSQLAndInterpolate(u)
 
 	fmt.Print("\n\n")
 
-	ce = &categoryEntity{678, 6, "p456", dbr.NullString{}}
+	ce = &categoryEntity{678, 6, "p456", dbr.NullString{}, nil}
 
 	// Updates only one row in the table. You can call SetRecord and Exec as
 	// often as you like. Each call to Exec will reassemble the arguments.
 	u = dbr.NewUpdate("catalog_category_entity").
-		AddColumns("attribute_set_id", "parent_id", "path").
+		AddColumns("attribute_set_id", "parent_id", "path", "teaser_id_s").
 		SetRecord(ce).
 		Where(dbr.Column("entity_id").PlaceHolder()) // No Arguments in Int64s because we need a place holder.
 	writeToSQLAndInterpolate(u)
@@ -75,19 +89,19 @@ func ExampleUpdate_SetRecord() {
 	// Output:
 	//Prepared Statement:
 	//UPDATE `catalog_category_entity` SET `attribute_set_id`=?, `parent_id`=?,
-	//`path`=?
-	//Arguments: [6 p123 4/5/6/7]
+	//`path`=?, `teaser_id_s`=?
+	//Arguments: [6 p123 4/5/6/7 saleAutumn|saleShoe]
 	//
 	//Interpolated Statement:
 	//UPDATE `catalog_category_entity` SET `attribute_set_id`=6, `parent_id`='p123',
-	//`path`='4/5/6/7'
+	//`path`='4/5/6/7', `teaser_id_s`='saleAutumn|saleShoe'
 	//
 	//Prepared Statement:
 	//UPDATE `catalog_category_entity` SET `attribute_set_id`=?, `parent_id`=?,
-	//`path`=? WHERE (`entity_id` = ?)
-	//Arguments: [6 p456 <nil> 678]
+	//`path`=?, `teaser_id_s`=? WHERE (`entity_id` = ?)
+	//Arguments: [6 p456 <nil> <nil> 678]
 	//
 	//Interpolated Statement:
 	//UPDATE `catalog_category_entity` SET `attribute_set_id`=6, `parent_id`='p456',
-	//`path`=NULL WHERE (`entity_id` = 678)
+	//`path`=NULL, `teaser_id_s`=NULL WHERE (`entity_id` = 678)
 }
