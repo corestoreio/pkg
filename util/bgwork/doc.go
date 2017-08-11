5333e97453b5697da72271d3600dbf1855a6d661
Cyrill Schumacher <cyrill@schumacher.fm>
2017-08-11 10:32:44 +0200

    Add pkg idea: util/bgwork

---
+++ util/bgwork/doc.go
@@ -0,0 +1,7 @@
+// Package bgwork handles background work or jobs distributed over many machines.
+//
+// TODO: gather ideas
+// - https://blog.gobuffalo.io/buffalo-v0-9-0-released-68fcf0844473
+// - https://github.com/gocraft/work
+// - https://medium.com/@matryer/introducing-vice-go-channels-across-many-machines-bcac1147d7e2
+package bgwork
