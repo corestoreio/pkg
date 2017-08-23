d77aa7ea78ebcdcdbda161ef6e98757a342a19d6
Cyrill Schumacher <cyrill@schumacher.fm>
2017-08-23 19:31:49 +0200

    Add new package codegen

---
+++ codegen/doc.go
@@ -0,0 +1,22 @@
+// Copyright 2015-2017, Cyrill @ Schumacher.fm and the CoreStore contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package codegen contains functions for writing database tables into Go structs.
+//
+// It supports package storage/dbr and its full functionality
+//
+// 3rd party packages which can support in code generation:
+// - https://github.com/fatih/astrewrite
+// - https://github.com/matroskin13/grizzly Collections generator for Golang
+package codegen
