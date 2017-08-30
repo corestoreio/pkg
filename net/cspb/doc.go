760fed80d7f52c5bd1dd722131dc401a04f85def
Cyrill Schumacher <cyrill@schumacher.fm>
2017-08-30 08:15:23 +0200

    net/cspb: Add package for protocol buffers, idea tank

---
+++ net/cspb/doc.go
@@ -0,0 +1,19 @@
+// Copyright 2015-2016, Cyrill @ Schumacher.fm and the CoreStore contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package cspb provides basics for CoreStore protocol buffer implementation.
+//
+// http://www.gophersre.com/2017/08/16/why-not-to-use-protos-in-code/
+// https://github.com/gogo/protobuf <-- Rely on this
+package cspb
