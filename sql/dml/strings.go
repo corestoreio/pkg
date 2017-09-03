3498be5fa343c1c7a176764f167040503cc0aa41
Cyrill Schumacher <cyrill@schumacher.fm>
2017-09-03 09:58:09 +0200

    Merge package util/dbh into sql/dml

--- sql/dml/strings.go
+++ sql/dml/strings.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package dbh
+package dml
 
 import (
 	"database/sql/driver"
@@ -23,7 +23,7 @@ import (
 	"github.com/corestoreio/errors"
 )
 
-// Strings contains several functions for data moveing. You can use package
+// Strings contains several functions for data moving. You can use package
 // slices.String for further modifications of this slice type.
 type Strings []string
 
--- sql/dml/strings_test.go
+++ sql/dml/strings_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package dbh_test
+package dml
 
 import "testing"
 
