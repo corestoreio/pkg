982261f2fa9f5ea3a63a835c995128559984952f
Cyrill Schumacher <cyrill@schumacher.fm>
2017-10-11 07:31:31 +0200

    sql/ddl: Add Variables.Typed functions and Variables.Keys
    Typed functions like Int64(key string) (i int64,ok bool) will parse the
    string into the desired type. On error or not found return variable ok
    will be false.
    
    Keys function might append the map keys to the input slice.

--- sql/ddl/variables.go
+++ sql/ddl/variables.go
@@ -15,6 +15,7 @@
 package ddl
 
 import (
+	"strconv"
 	"strings"
 
 	"github.com/corestoreio/csfw/sql/dml"
@@ -44,6 +45,65 @@ func NewVariables(names ...string) *Variables {
 	return vs
 }
 
+// Float64 returns for a given key its float64 value. If the key does not exists
+// or string parsing into float fails, it returns false.
+func (vs *Variables) Float64(key string) (val float64, ok bool) {
+	vals, ok := vs.Data[key]
+	if !ok {
+		return val, false
+	}
+	val, err := strconv.ParseFloat(vals, 64)
+	return val, err == nil
+}
+
+// Int64 returns for a given key its int64 value. If the key does not exists
+// or string parsing into int fails, it returns false.
+func (vs *Variables) Int64(key string) (val int64, ok bool) {
+	vals, ok := vs.Data[key]
+	if !ok {
+		return val, false
+	}
+	val, err := strconv.ParseInt(vals, 10, 64)
+	return val, err == nil
+}
+
+// Uint64 returns for a given key its uint64 value. If the key does not exists
+// or string parsing into uint fails, it returns false.
+func (vs *Variables) Uint64(key string) (val uint64, ok bool) {
+	vals, ok := vs.Data[key]
+	if !ok {
+		return val, false
+	}
+	val, err := strconv.ParseUint(vals, 10, 64)
+	return val, err == nil
+}
+
+// Bool returns for a given key its bool value. If the key does not exists or
+// string parsing into bool fails, it returns false. Only allowed bool values
+// are YES, NO, ON, OFF and yes, no, on, off.
+func (vs *Variables) Bool(key string) (val bool, ok bool) {
+	vals, ok := vs.Data[key]
+	if !ok {
+		return val, false
+	}
+	switch vals {
+	case "YES", "yes", "ON", "on":
+		val, ok = true, true
+	case "NO", "no", "OFF", "off":
+		val, ok = false, true
+	default:
+		ok = false
+	}
+	return
+}
+
+// String returns for a given key its string value. If the key does not exists,
+// it returns false.
+func (vs *Variables) String(key string) (val string, ok bool) {
+	val, ok = vs.Data[key]
+	return
+}
+
 // EqualFold reports whether the value of key and `expected`, interpreted as
 // UTF-8 strings, are equal under Unicode case-folding.
 func (vs *Variables) EqualFold(key, expected string) bool {
@@ -55,6 +115,19 @@ func (vs *Variables) Equal(key, expected string) bool {
 	return vs.Data[key] == expected
 }
 
+// Keys if argument keys has been provided the map keys will be appended to the
+// slice otherwise a new slice gets returned. The returned slice has a random
+// order. The keys argument is not for filtering.
+func (vs *Variables) Keys(keys ...string) []string {
+	if keys == nil {
+		keys = make([]string, 0, len(vs.Data))
+	}
+	for k := range vs.Data {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
 // ToSQL implements dml.QueryBuilder interface to assemble a SQL string and its
 // arguments for query execution.
 func (vs *Variables) ToSQL() (string, []interface{}, error) {
@@ -64,7 +137,6 @@ func (vs *Variables) ToSQL() (string, []interface{}, error) {
 // MapColumns implements dml.ColumnMapper interface and scans a single row from
 // the database query result.
 func (vs *Variables) MapColumns(rc *dml.ColumnMap) error {
-	// TODO: how to handle to load all variables stored in the map?
 	name, value := "", ""
 	for rc.Next() {
 		switch col := rc.Column(); col {
--- sql/ddl/variables_test.go
+++ sql/ddl/variables_test.go
@@ -16,6 +16,9 @@ package ddl
 
 import (
 	"context"
+	"math"
+	"sort"
+	"strconv"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
@@ -83,6 +86,9 @@ func TestNewVariables_Mock(t *testing.T) {
 		assert.Exactly(t, `helloAustralia`, vs.Data["keyVal11"])
 		assert.Exactly(t, `helloNewZealand`, vs.Data["keyVal22"])
 		assert.Len(t, vs.Data, 2)
+		keys := vs.Keys()
+		sort.Strings(keys)
+		assert.Exactly(t, []string{"keyVal11", "keyVal22"}, keys)
 	})
 }
 
@@ -98,4 +104,106 @@ func TestVariables_Equal(t *testing.T) {
 
 	assert.True(t, v.EqualFold("any_key", "a"))
 	assert.False(t, v.EqualFold("any_key", "B"))
+}
+
+func TestVariables_Types(t *testing.T) {
+	t.Parallel()
+
+	v := NewVariables("any_key")
+	v.Data["float64_ok"] = "3.14159"
+	v.Data["float64_nok"] = "ø"
+	v.Data["int64_ok"] = strconv.FormatInt(-math.MaxInt64, 10)
+	v.Data["int64_nok"] = "ø"
+	v.Data["uint64_ok"] = strconv.FormatUint(math.MaxUint64, 10)
+	v.Data["uint64_nok"] = "ø"
+
+	v.Data["bool_YES"] = "YES"
+	v.Data["bool_NO"] = "NO"
+	v.Data["bool_ON"] = "ON"
+	v.Data["bool_OFF"] = "OFF"
+	v.Data["bool_nok1"] = "1"
+	v.Data["bool_nok0"] = "0"
+
+	t.Run("string", func(t *testing.T) {
+		val, ok := v.String("float64_ok")
+		assert.True(t, ok)
+		assert.Exactly(t, "3.14159", val)
+
+		val, ok = v.String("not_found")
+		assert.False(t, ok)
+		assert.Empty(t, val)
+	})
+
+	t.Run("bool", func(t *testing.T) {
+		val, ok := v.Bool("bool_YES")
+		assert.True(t, ok)
+		assert.True(t, val)
+
+		val, ok = v.Bool("bool_NO")
+		assert.True(t, ok)
+		assert.False(t, val)
+
+		val, ok = v.Bool("bool_ON")
+		assert.True(t, ok)
+		assert.True(t, val)
+
+		val, ok = v.Bool("bool_OFF")
+		assert.True(t, ok)
+		assert.False(t, val)
+
+		val, ok = v.Bool("bool_nok1")
+		assert.False(t, ok)
+		assert.False(t, val)
+
+		val, ok = v.Bool("bool_nok0")
+		assert.False(t, ok)
+		assert.False(t, val)
+
+		val, ok = v.Bool("not_found")
+		assert.False(t, ok)
+		assert.False(t, val)
+	})
+
+	t.Run("float64", func(t *testing.T) {
+		val, ok := v.Float64("float64_ok")
+		assert.True(t, ok)
+		assert.Exactly(t, 3.14159, val)
+
+		val, ok = v.Float64("float64_nok")
+		assert.False(t, ok)
+		assert.Exactly(t, float64(0), val)
+
+		val, ok = v.Float64("not_found")
+		assert.False(t, ok)
+		assert.Exactly(t, float64(0), val)
+	})
+
+	t.Run("int64", func(t *testing.T) {
+		val, ok := v.Int64("int64_ok")
+		assert.True(t, ok)
+		assert.Exactly(t, int64(-math.MaxInt64), val)
+
+		val, ok = v.Int64("int64_nok")
+		assert.False(t, ok)
+		assert.Exactly(t, int64(0), val)
+
+		val, ok = v.Int64("not_found")
+		assert.False(t, ok)
+		assert.Exactly(t, int64(0), val)
+	})
+
+	t.Run("uint64", func(t *testing.T) {
+		val, ok := v.Uint64("uint64_ok")
+		assert.True(t, ok)
+		assert.Exactly(t, uint64(math.MaxUint64), val)
+
+		val, ok = v.Uint64("uint64_nok")
+		assert.False(t, ok)
+		assert.Exactly(t, uint64(0), val)
+
+		val, ok = v.Uint64("not_found")
+		assert.False(t, ok)
+		assert.Exactly(t, uint64(0), val)
+	})
+
 }
