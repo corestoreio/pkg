39d3338d6374636d5fa90b75ce105015c0d4524b
Cyrill Schumacher <cyrill@schumacher.fm>
2017-12-18 20:26:18 +0100

    Remove packages sqlparser and sqlbeautifier. Not needed.

--- eav/attribute_sql_test.go
+++ eav/attribute_sql_test.go
@@ -16,45 +16,8 @@ package eav_test
 
 import (
 	"testing"
-
-	"github.com/corestoreio/pkg/codegen"
-	"github.com/corestoreio/pkg/eav"
-	"github.com/corestoreio/pkg/storage/csdb"
-	"github.com/corestoreio/pkg/util/diff"
-	"github.com/corestoreio/pkg/util/sqlbeautifier"
-	"github.com/stretchr/testify/assert"
 )
 
-var testWantGetAttributeSelectSql string = "Please specify a build tag: mage1 or mage2\n$ go test -tags mageX -v ."
-
 func TestGetAttributeSelectSql(t *testing.T) {
-	dbc := csdb.MustConnectTest()
-	defer dbc.Close()
 
-	dbrSess := dbc.NewSession()
-	dbrSelect, err := eav.GetAttributeSelectSql(dbrSess, codegen.NewAddAttrTables(dbc.DB, "customer"), 1, 4)
-	if err != nil {
-		t.Fatal(err)
-	}
-	sql, _, err := dbrSelect.ToSQL()
-	assert.NoError(t, err)
-
-	_, err = sqlbeautifier.FromString(sql) // check for syntax errors
-	if err != nil {
-		t.Fatalf("%s\n\n%s\n", err, sql)
-	}
-
-	if testWantGetAttributeSelectSql != sql {
-
-		buf, err := sqlbeautifier.FromString(testWantGetAttributeSelectSql)
-		if err != nil {
-			t.Fatalf("%s\n%s\n", err, testWantGetAttributeSelectSql)
-		}
-		sql = sqlbeautifier.MustFromString(sql)
-		println(diff.MustUnified(buf.String(), sql), "\n")
-		t.Fatal(sql)
-	}
-
-	// @todo error is that we have column attribute_model in the select list but it should not occur
-	// because in codegen it is defined that this column has no usage so we can skip it.
 }
