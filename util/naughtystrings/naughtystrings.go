0551ffade681dd7e6bda3454751b1a718936c945
Cyrill Schumacher <cyrill@schumacher.fm>
2017-12-21 07:19:58 +0100

    util/naughtystrings: Lazy, not thread safe, loading of the data

--- util/naughtystrings/naughtystrings.go
+++ util/naughtystrings/naughtystrings.go
@@ -2,7 +2,8 @@
 //go:generate go install github.com/jteeuwen/go-bindata/go-bindata
 //go:generate go-bindata -ignore \.git\S* -ignore LICENSE -ignore README\.md -ignore blns\.base64\.txt -ignore blns\.txt -ignore package\.json -o internal/resource.go -nocompress -pkg internal ..
 
-// Package naughtystrings is a collection of strings that have a high probability of causing issues when used as user input.
+// Package naughtystrings is a collection of strings that have a high
+// probability of causing issues when used as user input.
 package naughtystrings
 
 import (
@@ -15,17 +16,20 @@ var base64encoded, unencoded []string
 
 // Base64Encoded returns the strings encoded in base 64.
 func Base64Encoded() []string {
+	// TODO make thread safe!
+	if base64encoded == nil {
+		base64encoded = load("../blns.base64.json")
+	}
 	return base64encoded
 }
 
 // Unencoded returns the strings.
 func Unencoded() []string {
+	// TODO make thread safe!
+	if unencoded == nil {
+		unencoded = load("../blns.json")
+	}
 	return unencoded
-}
-
-func init() {
-	base64encoded = load("../blns.base64.json")
-	unencoded = load("../blns.json")
 }
 
 func load(file string) []string {
