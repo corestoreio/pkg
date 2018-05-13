7c97d59519f2ad3ffa93870bf556152954e93375
Cyrill Schumacher <cyrill@schumacher.fm>
2018-05-13 19:00:58 +0200

    config/storage/cfgfile: Add more tests for WithLoadJSON

--- config/storage/cfgfile/json_test.go
+++ config/storage/cfgfile/json_test.go
@@ -17,6 +17,7 @@ package cfgfile_test
 import (
 	"testing"
 
+	"github.com/corestoreio/errors"
 	"github.com/corestoreio/pkg/config"
 	"github.com/corestoreio/pkg/config/storage"
 	"github.com/corestoreio/pkg/config/storage/cfgfile"
@@ -24,27 +25,53 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestWithLoadData_Success(t *testing.T) {
+func TestWithLoadJSON(t *testing.T) {
+	pUserName := config.MustNewPath("payment/stripe/user_name")
 
-	inMem := storage.NewMap()
-	cfgSrv, err := config.NewService(
-		inMem, config.Options{},
-		cfgfile.WithLoadJSON("testdata/example.json"),
-	)
-	if err != nil {
-		t.Fatalf("%+v", err)
+	t.Run("success", func(t *testing.T) {
+
+		cfgSrv, err := config.NewService(
+			storage.NewMap(), config.Options{},
+			cfgfile.WithLoadJSON("testdata/example.json"),
+		)
+		if err != nil {
+			t.Fatalf("%+v", err)
+		}
+
+		assert.Exactly(t, `"AUserName"`, cfgSrv.Get(pUserName).String())
+		assert.Exactly(t, `"WS0Username"`, cfgSrv.Get(pUserName.BindWebsite(0)).String())
+		assert.Exactly(t, `"WS1Username"`, cfgSrv.Get(pUserName.BindWebsite(1)).String())
+		assert.Exactly(t, `"WS2Username"`, cfgSrv.Get(pUserName.BindWebsite(2)).String())
+
+		assert.Exactly(t, `"SO5Username"`, cfgSrv.Get(pUserName.BindStore(5)).String())
+		assert.Exactly(t, `"SO11Username"`, cfgSrv.Get(pUserName.BindStore(11)).String())
+
+		assert.Exactly(t, `"1234"`, cfgSrv.Get(config.MustNewPath("payment/stripe/port")).String())
+		assert.Exactly(t, `"true"`, cfgSrv.Get(config.MustNewPathWithScope(scope.Website.WithID(0), "payment/stripe/enable")).String())
+	})
+
+	runner := func(file string, errKind errors.Kind, errTxt string) func(*testing.T) {
+		return func(t *testing.T) {
+			cfgSrv, err := config.NewService(
+				storage.NewMap(), config.Options{},
+				cfgfile.WithLoadJSON("testdata/"+file),
+			)
+			assert.True(t, errKind.Match(err), "%+v", err)
+			assert.Nil(t, cfgSrv)
+			assert.Contains(t, err.Error(), errTxt)
+			//t.Logf("%+v", err)
+		}
 	}
-	p := config.MustNewPath("payment/stripe/user_name")
+	t.Run("malformed_v1", runner("malformed_v1.json", errors.CorruptData,
+		"[cfgjson] Unexpected data in \"payment/stripe/port\""))
 
-	assert.Exactly(t, `"AUserName"`, cfgSrv.Get(p).String())
-	assert.Exactly(t, `"WS0Username"`, cfgSrv.Get(p.BindWebsite(0)).String())
-	assert.Exactly(t, `"WS1Username"`, cfgSrv.Get(p.BindWebsite(1)).String())
-	assert.Exactly(t, `"WS2Username"`, cfgSrv.Get(p.BindWebsite(2)).String())
+	t.Run("malformed_v2_dataIF", runner("malformed_v2_dataIF.json", errors.CorruptData,
+		"Unable to cast map[string]interface {}{} to []byte"))
 
-	assert.Exactly(t, `"SO5Username"`, cfgSrv.Get(p.BindStore(5)).String())
-	assert.Exactly(t, `"SO11Username"`, cfgSrv.Get(p.BindStore(11)).String())
+	t.Run("malformed_v2_dataIF_scopeID", runner("malformed_v2_dataIF_scopeID.json", errors.CorruptData,
+		`failed to parse "-1" to uint: strconv.ParseUint: parsing "-1": invalid syntax`))
 
-	assert.Exactly(t, `"1234"`, cfgSrv.Get(config.MustNewPath("payment/stripe/port")).String())
-	assert.Exactly(t, `"true"`, cfgSrv.Get(config.MustNewPathWithScope(scope.Website.WithID(0), "payment/stripe/enable")).String())
+	t.Run("malformed_v2t_dataIF", runner("malformed_v2t_dataIF.json", errors.CorruptData,
+		`Unexpected data in []interface {}{}`))
 
 }
