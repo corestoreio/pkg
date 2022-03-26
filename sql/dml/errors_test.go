7d336d20b839a3ada7907bcf6b3d7765bb39e722
Cyrill Schumacher <cyrill@schumacher.fm>
2022-03-26 20:49:14 +0100

    sql/dml: Add general error type

--- sql/dml/errors_test.go
+++ sql/dml/errors_test.go
@@ -1,21 +1,23 @@
 package dml
 
 import (
+	"fmt"
 	"testing"
 
-	"github.com/corestoreio/errors"
 	"github.com/corestoreio/pkg/util/assert"
 	"github.com/go-sql-driver/mysql"
 )
+
+var _ error = (*Error)(nil)
 
 func TestMySQLFromError(t *testing.T) {
 	myErr := &mysql.MySQLError{
 		Number:  1062,
 		Message: "Duplicate Key",
 	}
-	haveM := MySQLMessageFromError(errors.Fatal.New(myErr, "Outer fatal error"))
+	haveM := MySQLMessageFromError(fmt.Errorf("outer fatal error: %w", myErr))
 	assert.Exactly(t, "Duplicate Key", haveM)
 
-	haveN := MySQLNumberFromError(errors.Fatal.New(myErr, "Outer fatal error"))
+	haveN := MySQLNumberFromError(fmt.Errorf("outer fatal error: %w", myErr))
 	assert.Exactly(t, uint16(1062), haveN)
 }
